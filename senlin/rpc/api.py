8801ff03c90ecf37de50db252c44da4d78dbb2ba
Yanyan Hu <yanyanhu@cn.ibm.com>
2014-12-11 04:28:18 -0500

    Initial version -- rpc
    May need to add rpc parameters definition for other Senlin
    conceptions in rpc/api.py, e.g. policy, member, webhook.

--- senlin/rpc/api.py
+++ senlin/rpc/api.py
@@ -22,7 +22,6 @@ PARAM_KEYS = (
 CLUSTER_KEYS = (
     CLUSTER_NAME, CLUSTER_ID,
     CLUSTER_CREATION_TIME, CLUSTER_UPDATED_TIME, CLUSTER_DELETION_TIME,
-    CLUSTER_NOTIFICATION_TOPICS,
     CLUSTER_DESCRIPTION, CLUSTER_ACTION,
     CLUSTER_STATUS, CLUSTER_STATUS_DATA, CLUSTER_CAPABILITIES,
     CLUSTER_TIMEOUT, CLUSTER_OWNER,
@@ -30,7 +29,6 @@ CLUSTER_KEYS = (
 ) = (
     'cluster_name', 'cluster_identity',
     'creation_time', 'updated_time', 'deletion_time',
-    'notification_topics',
     'description', 'cluster_action',
     'cluster_status', 'cluster_status_reason', 'capabilities',
     'timeout_mins', 'cluster_owner',
