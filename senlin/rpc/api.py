95c867c38d2c34cd1d9a493d16076f12813297c7
tengqm <tengqim@cn.ibm.com>
2014-12-18 00:29:56 +0800

    Working version
    May need to rename this file

--- senlin/rpc/api.py
+++ senlin/rpc/api.py
@@ -14,15 +14,25 @@
 ENGINE_TOPIC = 'engine'
 
 CLUSTER_KEYS = (
-    CLUSTER_NAME, CLUSTER_PROFILE, CLUSTER_SIZE,
-    CLUSTER_UUID, CLUSTER_PARENT,
+    CLUSTER_NAME, CLUSTER_PROFILE,
+    CLUSTER_ID, CLUSTER_PARENT,
     CLUSTER_DOMAIN, CLUSTER_PROJECT, CLUSTER_USER,
     CLUSTER_CREATED_TIME, CLUSTER_UPDATED_TIME, CLUSTER_DELETED_TIME,
     CLUSTER_STATUS, CLUSTER_STATUS_REASON, CLUSTER_TIMEOUT,
+    CLUSTER_TAGS,
 ) = (
-    'name', 'profile', 'size',
-    'uuid', 'parent',
+    'name', 'profile_id',
+    'id', 'parent',
     'domain', 'project', 'user',
     'created_time', 'updated_time', 'deleted_time',
-    'status', 'status_reason', 'timeout'
+    'status', 'status_reason', 'timeout',
+    'tags',
+)
+
+EVENT_KEYS = (
+    EVENT_TIMESTAMP, EVENT_OBJ_ID, EVENT_OBJ_NAME, EVENT_OBJ_TYPE,
+    EVENT_USER, EVENT_ACTION, EVENT_STATUS, EVENT_STATUS_REASON,
+) = (
+    'timestamp', 'obj_id', 'obj_name', 'obj_type',
+    'user', 'action', 'status', 'status_reason',
 )
