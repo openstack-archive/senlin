60ca1706a45a01b077769ad211a001c81e35c703
tengqm <tengqim@cn.ibm.com>
2014-12-17 16:19:47 +0800

    Removed profiler support

--- senlin/common/messaging.py
+++ senlin/common/messaging.py
@@ -14,7 +14,6 @@ import eventlet
 from oslo.config import cfg
 import oslo.messaging
 from oslo.serialization import jsonutils
-from osprofiler import profiler
 
 from senlin.common import context
 
@@ -45,22 +44,10 @@ class RequestContextSerializer(oslo.messaging.Serializer):
 
     @staticmethod
     def serialize_context(ctxt):
-        _context = ctxt.to_dict()
-        prof = profiler.get()
-        if prof:
-            trace_info = {
-                "hmac_key": prof.hmac_key,
-                "base_id": prof.get_base_id(),
-                "parent_id": prof.get_id()
-            }
-            _context.update({"trace_info": trace_info})
-        return _context
+        return ctxt.to_dict()
 
     @staticmethod
     def deserialize_context(ctxt):
-        trace_info = ctxt.pop("trace_info", None)
-        if trace_info:
-            profiler.init(**trace_info)
         return context.RequestContext.from_dict(ctxt)
 
 
