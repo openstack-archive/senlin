edd5080efa47c23cfb2fc89d43008d275739b21e
tengqm <tengqim@cn.ibm.com>
2015-01-02 00:20:44 +0800

    Rewriting base Profile class
    - Revised __new__ method to allow for polymorphism;
    - Added DB 'serialization' supports;
    - Simplified object life cycle methods.

--- senlin/profiles/base.py
+++ senlin/profiles/base.py
@@ -10,64 +10,99 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import collections
-import uuid
 
+from senlin.common import exception
+from senlin.db import api as db_api
+from senlin.engine import environment
 from senlin.openstack.common import log as logging
 
 LOG = logging.getLogger(__name__)
 
 
-class ProfileBase(object):
+class Profile(object):
     '''
     Base class for profiles.
     '''
-    def __new__(cls, profile, *args, **kwargs):
+    def __new__(cls, type_name, name, **kwargs):
         '''
         Create a new profile of the appropriate class.
         '''
-        global _profile_classes
-        if _profile_classes is None:
-            mgr = extension.ExtensionManager(name_space='senlin.profiles',
-                                             invoke_on_load=False,
-                                             verify_requirements=True)
-            _profile_classes = dict((tuple(name.split('.')), mgr[name].plugin)
-                                    for name in mgr.names())
-
-        if cls != ProfileBase:
+        if cls != Profile:
             ProfileClass = cls
         else:
-            ProfileClass = get_profile_class(profile)
+            ProfileClass = environment.global_env().get_profile(type_name)
 
         return super(Profile, cls).__new__(ProfileClass)
 
-    def __init__(self, name, type_name, **kwargs):
+    def __init__(self, type_name, name, **kwargs):
         '''
         Initialize the profile with given parameters and a JSON object.
         '''
         self.name = name
-        self.type_name = type_name
-        self.permission = ''
-        self.spec = kwargs.get('spec')
-        self.tags = {}
+        self.type = type_name
+        self.id = kwargs.get('id', None)
+        self.permission = kwargs.get('permission', '')
+        self.spec = kwargs.get('spec', {})
+        self.tags = kwargs.get('tags', {})
+        self.deleted_time = kwargs.get('deleted_time', None)
 
     @classmethod
-    def create_object(cls, name, type_name, **kwargs):
-        obj = cls(name, type_name, kwargs)
-        physical_id = obj.do_create()
-        return physical_id
+    def from_db_record(cls, context, record):
+        '''
+        Construct a profile object from database record.
+        :param context: the context used for DB operations.
+        :param record: a DB Profle object that contains all required fields.
+        '''
+        kwargs = {
+            'id': record.id,
+            'spec': record.spec,
+            'permission': record.permission,
+            'tags': record.tags,
+            'deleted_time': record.deleted_time,
+        }
+
+        return cls(record.type, record.name, **kwargs)
 
     @classmethod
-    def delete_object(cls, physical_id):
-        obj = db_api.load_member(physical_id=physical_id)
-        result = obj.do_delete()
-        return result
+    def load(cls, context, profile_id):
+        '''
+        Retrieve a profile object from database.
+        '''
+        profile = db_api.profile_get(context, profile_id)
+        if profile is None:
+            msg = _('No profile with id "%s" exists') % profile_id
+            raise exception.NotFound(msg)
+
+        return cls.from_db_record(context, profile)
+
+    def store(self):
+        '''
+        Store the profile into database and return its ID.
+        '''
+        values = {
+            'name': self.name,
+            'type': self.type,
+            'spec': self.spec,
+            'permission': self.permission,
+            'tags': self.tags,
+        }
+        profile = db_api.profile_create(self.context, values)
+        return profile.id
 
     @classmethod
-    def update_object(cls, physical_id, new_profile):
-        obj = db_api.load_member(physical_id=physical_id)
-        result = obj.do_update()
-        return result
+    def create_object(cls, obj):
+        profile = cls.from_db(obj.context, obj.profile_id)
+        return profile.do_create(obj)
+
+    @classmethod
+    def delete_object(cls, obj):
+        profile = cls.from_db(obj.context, obj.profile_id)
+        return profile.do_delete(obj)
+
+    @classmethod
+    def update_object(cls, obj, new_profile_id):
+        profile = cls.from_db(obj.context, obj.profile_id)
+        return profile.do_update(obj, new_profile_id)
 
     def do_create(self):
         '''
@@ -99,5 +134,4 @@ class ProfileBase(object):
 
     @classmethod
     def from_dict(cls, **kwargs):
-        pb = cls(kwargs)
-        return pb
+        return cls(kwargs)
