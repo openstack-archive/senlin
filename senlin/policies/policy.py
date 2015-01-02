8157f4b5a09def57bc4076f84b6f4c48cb45c5c5
tengqm <tengqim@cn.ibm.com>
2015-01-02 10:48:36 +0800

    Added DB serialization logics

--- senlin/policies/policy.py
+++ senlin/policies/policy.py
@@ -10,34 +10,111 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from senlin.common import exception
+from senlin.db import api as db_api
+from senlin.engine import environment
+
 
 class Policy(object):
     '''
     Base class for policies.
     '''
-    def __init__(self, name, type_name, **kwargs):
+    ENFORCEMENT_LEVELS = (
+        CRITICAL, ERROR, WARNING, INFO, DEBUG,
+    ) = (
+        'CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG',
+    )
+
+    def __new__(cls, type_name, name, **kwargs):
+        '''
+        Create a new policy of the appropriate class.
+        '''
+        if cls != Policy:
+            PolicyClass = cls
+        else:
+            PolicyClass = environment.global_env().get_policy(type_name)
+
+        return super(Policy, cls).__new__(PolicyClass)
+
+    def __init__(self, type_name, name, **kwargs):
+        self.id = kwargs.get('id', None)
         self.name = name
         self.type = type_name
-        self.cooldown = 0
-        self.level = DEBUG
-        self.spec = {}
-        self.data = {}
 
-    def pre_op(self, cluster_id, action, **args):
+        self.context = kwargs.get('context', None)
+        self.cooldown = kwargs.get('cooldown', 0)
+        self.level = kwargs.get('level', self.DEBUG)
+        self.spec = kwargs.get('spec', {})
+        self.data = kwargs.get('data', {})
+
+    def store(self):
+        '''
+        Store the policy object into database table.
+        This could be a policy_create or a policy_update DB API invocation,
+        depends on whether self.id is set.
+        '''
+        values = {
+            'name': self.name,
+            'type': self.type,
+            'cooldown': self.cooldown,
+            'level': self.level,
+            'spec': self.spec,
+            'data': self.data,
+        }
+
+        if self.id:
+            db_api.policy_update(self.context, self.id, values)
+        else:
+            policy = db_api.policy_create(self.context, values)
+            self.id = policy.id
+        return self.id
+
+    @classmethod
+    def from_db_record(cls, context, record):
+        '''
+        Construct a policy object from a database record.
+        :param context:
+        :param record:
+        '''
+        kwargs = {
+            'id': record.id,
+            'name': record.name,
+            'context': context,
+            'type': record.type,
+            'cooldown': record.cooldown,
+            'level': record.level,
+            'spec': record.spec,
+            'data': record.data,
+        }
+        return cls(record.type, record.name, **kwargs)
+
+    @classmethod
+    def load(cls, context, policy_id):
+        '''
+        Retrieve and reconstruct a policy object from DB.
+        '''
+        policy = db_api.policy_get(context, policy_id)
+        if policy is None:
+            msg = _('No policy with id "%s" exists') % policy_id
+            raise exception.NotFound(msg)
+
+        return cls.from_db_record(context, policy)
+
+    def pre_op(self, cluster_id, action, **kwargs):
         '''
         Force all subclasses to implement an operation that will be invoked
         before an action.
         '''
         return NotImplemented
 
-    def enforce(self, cluster_id, action, **args):
+    def enforce(self, cluster_id, action, **kwargs):
         '''
         Force all subclasses to implement an operation that can be called
         during an action.
         '''
         return NotImplemented
 
-    def post_op(self, cluster_id, action, **args):
+    def post_op(self, cluster_id, action, **kwargs):
         '''
         Force all subclasses to implement an operation that will be performed
         after an action.
@@ -46,16 +123,18 @@ class Policy(object):
 
     def to_dict(self):
         pb_dict = {
+            'id': self.id,
             'name': self.name,
-            'type': self.type_name,
-            'uuid': self.uuid,
+            'type': self.type,
             'spec': self.spec,
             'level': self.level,
             'cooldown': self.cooldown,
+            'data': self.data,
         }
         return pb_dict
 
     @classmethod
-    def from_dict(self, **kwargs):
-        pb = PolicyBase(kwargs)
-        return pb
+    def from_dict(cls, **kwargs):
+        type_name = kwargs.get('type', '')
+        name = kwargs.get('name', '')
+        return cls(type_name, name, **kwargs)
