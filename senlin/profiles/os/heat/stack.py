7191644be3b5b56e305d5fee35f476743a11c629
tengqm <tengqim@cn.ibm.com>
2015-01-03 12:11:17 +0800

    Stack profile implementation draft
    Added some methods so that Heat stack can be created/deleted/updated as
    profile operations are issued.

--- senlin/profiles/os/heat/stack.py
+++ senlin/profiles/os/heat/stack.py
@@ -10,33 +10,168 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import uuid
+import six
 
-from senlin.drivers import heat_v1 as heat
+from senlin.common import context
+from senlin.common import exception
 from senlin.profiles import base
 
-__type_name__ = 'os.heat.stack'
+__PROFILE_TYPE__ = 'os.heat.stack'
 
 
-class StackProfile(base.ProfileBase):
+class StackProfile(base.Profile):
     '''
     Profile for an OpenStack Heat stack.
     When this profile is used, the whole cluster is a collection of Heat
     stacks.
     '''
-    def __init__(self, name, type_name=__type_name__, **kwargs):
-        super(StackProfile, self).__init__(name, type_name, kwargs)
 
-        self.enable_rollback = kwargs.get('enable_rollback')
-        self.timeout = kwargs.get('timeout') or 60
+    KEYS = (
+        TEMPLATE, CONTEXT, PARAMETERS,
+        TIMEOUT, ENABLE_ROLLBACK,
+    ) = (
+        'template', 'context', 'parameters',
+        'timeout', 'enable_rollback'
+    )
 
-    def do_create(self):
-        return stack
+    def __init__(self, type_name, name, **kwargs):
+        super(StackProfile, self).__init__(type_name, name, **kwargs)
 
-    def do_delete(self):
+        self.template = self.spec.get('template', {})
+        self.profile_context = self.spec.get('context', {})
+        self.parameters = self.spec.get('parameters', {})
+        self.disable_rollback = self.spec.get('disable_rollback', True)
+        self.timeout = self.spec.get('timeout', 60)
+
+        self.hc = None
+        self.stack_context = None
+        self.stack_id = None
+
+    def heat(self, obj):
+        '''
+        Construct heat client using the combined context.
+        '''
+        if self.hc:
+            return self.hc
+
+        ctx = obj.context
+        if self.profile_context:
+            ctx.update(self.profile_context)
+        self.stack_context = context.RequestContext.from_dict(ctx)
+        self.hc = self.stack_context.clients.heat()
+
+    def do_validate(self, obj):
+        '''
+        Validate if the spec has provided reasonable info for stack creation.
+        '''
+        kwargs = {
+            'stack_name': obj.name,
+            'template': self.template,
+            'timeout_mins': self.timeout,
+            'disable_rollback': self.disable_rollback,
+            'parameters': self.parameters,
+            # 'files':
+            'environment': {}
+        }
+        try:
+            self.heat().stacks.validate(**kwargs)
+        except Exception as ex:
+            msg = _('Failed validate stack template due to '
+                    '"%s"') % six.text_type(ex)
+            raise exception.ProfileValidationFailed(message=msg)
+
         return True
 
-    def do_update(self, ):
-        self.status = self.UPDATING
-        self.status = self.ACTIVE
+    def _check_action_complete(self, obj, action):
+        stack = self.heat(obj).stacks.get(stack_id=self.stack_id)
+        if stack.action == action:
+            if stack.status == 'IN_PROGRESS':
+                return False
+
+            if stack.status == 'COMPLETE':
+                return True
+
+            if stack.status == 'FAILED':
+                raise exception.NodeStatusError(
+                    status=stack.stack_status,
+                    reason=stack.stack_status_reason)
+            else:
+                raise exception.NodeStatusError(
+                    status=stack.stack_status,
+                    reason=stack.stack_status_reason)
+        else:
+            msg = _('Node action mismatch detected: expected=%(expected)s '
+                    'actual=%(actual)s') % dict(expected=action,
+                                                actual=stack.action)
+            raise exception.NodeStatusError(status=stack.stack_status,
+                                            reason=msg)
+
+    def do_create(self, obj):
+        '''
+        Create a stack using the given profile.
+        '''
+        kwargs = {
+            'stack_name': obj.name,
+            'template': self.template,
+            'timeout_mins': self.timeout,
+            'disable_rollback': self.disable_rollback,
+            'parameters': self.parameters,
+            # 'files':
+            'environment': {}
+        }
+
+        stack = self.heat(obj).stacks.create(**kwargs)
+        self.stack_id = stack['stack']['id']
+
+        # Wait for action to complete/fail
+        while not self._check_action_complete(obj, 'CREATE'):
+            yield
+        return True
+
+    def do_delete(self, obj):
+        self.stack_id = obj.physical_id
+
+        if not self.stack_id:
+            return True
+
+        try:
+            self.heat(obj).stacks.delete(stack_id=self.stack_id)
+        except Exception as ex:
+            if isinstance(ex, exception.NotFound):
+                pass
+            raise ex
+
+        # Wait for action to complete/fail
+        while not self._check_action_complete(obj, 'DELETE'):
+            yield
+
+        return True
+
+    def do_update(self, obj, new_profile):
+        '''
+        :param obj: the node object to operate on
+        :param new_profile: the new profile used for updating
+        '''
+        self.stack_id = obj.physical_id
+        if not self.stack_id:
+            return True
+
+        # TODO(anyone): Check if template differs
+        # TODO(anyone): Check if params differs
+        fields = {
+            'stack_id': self.stack_id,
+            'parameters': new_profile.params,
+            'template': new_profile.template,
+            'timeout_mins': new_profile.timeout,
+            'disable_rollback': new_profile.disable_rollback,
+            #'files': self.stack.t.files,
+            'environment': {},
+        }
+
+        self.heat(obj).stacks.update(**fields)
+
+        # Wait for action to complete/fail
+        while not self._check_action_complete(obj, 'UPDATE'):
+            yield
+
         return True
