3aa5921f142a4d4781874108a166b8824835f176
tengqm <tengqim@cn.ibm.com>
2015-01-15 10:59:36 +0800

    Fix status field error
    We don't have {ACTION}_{STATUS} form of status field.  This code was not
    right.

--- senlin/api/openstack/v1/views/clusters_view.py
+++ senlin/api/openstack/v1/views/clusters_view.py
@@ -1,15 +1,14 @@
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+
 #         http://www.apache.org/licenses/LICENSE-2.0
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
 import itertools
 
@@ -47,18 +46,7 @@ def format_cluster(req, cluster, keys=None, tenant_safe=True):
             yield ('links', [util.make_link(req, value)])
             if not tenant_safe:
                 yield ('project', value['tenant'])
-        elif key == rpc_api.CLUSTER_ACTION:
-            return
-        elif (key == rpc_api.CLUSTER_STATUS and
-              rpc_api.CLUSTER_ACTION in cluster):
-            # To avoid breaking API compatibility, we join RES_ACTION
-            # and RES_STATUS, so the API format doesn't expose the
-            # internal split of state into action/status
-            yield (key, '_'.join((cluster[rpc_api.CLUSTER_ACTION], value)))
         else:
-            # TODO(zaneb): ensure parameters can be formatted for XML
-            #elif key == rpc_api.CLUSTER_PARAMETERS:
-            #    return key, json.dumps(value)
             yield (key, value)
 
     return dict(itertools.chain.from_iterable(
