4d559c944f5c8a602655f0b63ce20b0536640eab
tengqm <tengqim@cn.ibm.com>
2015-01-27 20:38:14 +0800

    Partially rework cluster action module
    Changes include:
    1. Added a _wait_for_action() function that can be shared among actions.
    2. Fixed action lock return value checking where the return value could
       be None.
    3. Added action.store() calls after creation.
    4. Unified action naming for engine derived actions.
    5. Removed action list when generating new action to wait on.
    6. Moved policy_check() to execute() function to be shared.
    7. Removed cancel update logic because we don't need it at the moment.

--- senlin/engine/actions/cluster_action.py
+++ senlin/engine/actions/cluster_action.py
@@ -16,6 +16,7 @@ from senlin.common import exception
 from senlin.common.i18n import _
 from senlin.db import api as db_api
 from senlin.engine.actions import base
+from senlin.engine import cluster as clusterm
 from senlin.engine import dispatcher
 from senlin.engine import node as nodes
 from senlin.engine import scheduler
@@ -44,10 +45,30 @@ class ClusterAction(base.Action):
     def __init__(self, context, action, **kwargs):
         super(ClusterAction, self).__init__(context, action, **kwargs)
 
+    def _wait_for_action(self):
+        while self.get_status() != self.READY:
+            if scheduler.action_cancelled(self):
+                # During this period, if cancel request come, cancel this
+                # cluster operation immediately, then release the cluster
+                # lock and return.
+                LOG.debug(_('%(action)s %(id)s cancelled') % {
+                    'action': self.action, 'id': self.id})
+                return self.RES_CANCEL
+            elif scheduler.action_timeout(self):
+                # Action timeout, return
+                LOG.debug(_('%(action)s %(id)s timeout') % {
+                    'action': self.action, 'id': self.id})
+                return self.RES_TIMEOUT
+
+            # Continue waiting (with reschedule)
+            scheduler.reschedule(self, sleep=0)
+
+        return self.RES_OK
+
     def do_create(self, cluster):
         # Try to lock cluster first
         action_id = db_api.cluster_lock_create(cluster.id, self.id)
-        if action_id != self.id:
+        if action_id is not None and action_id != self.id:
             # This cluster has been locked by other action?
             # This should never happen here, raise an execption.
             LOG.error(_('Cluster has been locked by %s before creation.'),
@@ -55,21 +76,21 @@ class ClusterAction(base.Action):
             msg = _('Cluster is already locked by action %(old)s, action '
                     '%(new)s failed grabbing the lock') % {
                         'old': action_id, 'new': self.id}
+            # TODO(Qiming): Decide how to handle this situation
             raise exception.Error(msg)
 
-        res = cluster.do_create()
+        res = cluster.do_create(self.context)
         if not res:
             cluster.set_status(cluster.ERROR, 'Cluster creation failed')
             db_api.cluster_lock_release(cluster.id, self.id)
             return self.RES_ERROR
 
-        action_list = []
         for m in range(cluster.size):
             name = 'node-%003d' % m
             node = nodes.Node(name, cluster.profile_id, cluster.id)
-            node.store()
+            node.store(self.context)
             kwargs = {
-                'name': 'node-create-%003d' % m,
+                'name': 'node_create_%s' % node.id[:8],
                 'context': self.context,
                 'target': node.id,
                 'cause': 'Cluster creation',
@@ -78,59 +99,43 @@ class ClusterAction(base.Action):
             action = base.Action(self.context, 'NODE_CREATE', **kwargs)
             action.store(self.context)
 
-            action_list.append(action.id)
-
+            # Build dependency and make the new action ready
             db_api.action_add_dependency(action.id, self.id)
             action.set_status(self.READY)
 
-        # Notify dispatcher
-        for action_id in action_list:
-            dispatcher.notify(self.context,
-                              dispatcher.Dispatcher.NEW_ACTION,
-                              None,
-                              action_id=action_id)
+            dispatcher.notify(self.context, dispatcher.Dispatcher.NEW_ACTION,
+                              None, action_id=action_id)
 
         # Wait for cluster creating complete
-        # TODO(anyone): need db support
-        while self.get_status() != self.READY:
-            if scheduler.action_cancelled(self):
-                # During this period, if cancel request come,
-                # cancel this cluster creating immediately, then
-                # release the cluster lock and return.
-                LOG.debug('Cluster creation action %s cancelled' % self.id)
-                cluster.set_status(cluster.ERROR, 'Cluster creation cancelled')
-                db_api.cluster_lock_release(cluster.id, self.id)
-                return self.RES_CANCEL
-            elif scheduler.action_timeout(self):
-                # Action timeout, return
-                LOG.debug('Cluster creating action %s timeout' % self.id)
-                cluster.set_status(cluster.ERROR, 'Cluster creating timeout')
-                db_api.cluster_lock_release(cluster.id, self.id)
-                return self.RES_TIMEOUT
+        result = self.RES_OK
+        if cluster.size > 0:
+            result = self._wait_for_action()
 
-            # Continue waiting (with reschedule)
-            scheduler.reschedule(self, sleep=0)
+        if result == self.RES_OK:
+            cluster.set_status(self.context, cluster.ACTIVE,
+                               'Cluster creation completed')
+        elif result == self.RES_CANCEL:
+            cluster.set_status(self.context, cluster.ERROR,
+                               'Cluster creation cancelled')
+        elif result == self.RES_TIMEOUT:
+            cluster.set_status(self.context, cluster.ERROR,
+                               'Cluster creation timeout')
+        else:
+            # RETRY
+            pass
 
-        cluster.set_status(cluster.ACTIVE, 'Cluster creation completed')
         db_api.cluster_lock_release(cluster.id, self.id)
-
-        return self.RES_OK
+        return result
 
     def do_update(self, cluster, new_profile_id):
         # Try to lock cluster first
-        worker_id = db_api.cluster_lock_create(cluster.id, self.id)
-        if worker_id != self.id:
+        owner = db_api.cluster_lock_create(cluster.id, self.id)
+        if owner != self.id:
             # This cluster has been locked by other action.
             # We can't update this cluster, cancel this action.
-            LOG.debug('Cluster has been locked by action %s' % worker_id)
-            return self.RES_CANCEL
+            LOG.debug('Cluster has been locked by action %s' % owner)
+            return self.RES_RETRY
 
-        old_profile_id = cluster.profile_id
-        # Note: we need clean the node count of cluster each time
-        # cluster.do_update is invoked; Then this count could be added
-        # gradually each time a node update action finished. This helps
-        # us to track the progress of cluster updating.
-        # TODO(anyone): update this comment
         res = cluster.do_update(self.context, profile_id=new_profile_id)
         if not res:
             cluster.set_status(cluster.ACTIVE,
@@ -139,11 +144,10 @@ class ClusterAction(base.Action):
             return self.RES_ERROR
 
         # Create NodeActions for all nodes
-        action_list = []
         node_list = cluster.get_nodes()
         for node_id in node_list:
             kwargs = {
-                'name': 'node-update-%s' % node_id,
+                'name': 'node_update_%s' % node_id[:8],
                 'context': self.context,
                 'target': node_id,
                 'cause': 'Cluster update',
@@ -152,162 +156,70 @@ class ClusterAction(base.Action):
                 }
             }
             action = base.Action(self.context, 'NODE_UPDATE', **kwargs)
-            action_list.append(action)
-            action.set_status(self.READY)
+            action.store()
 
-            # add new action to waiting/dependency list
-            # TODO(anyone): need db_api support
             db_api.action_add_dependency(action, self)
-
-        # Notify dispatcher
-        for action in action_list:
-            dispatcher.notify(self.context,
-                              dispatcher.Dispatcher.NEW_ACTION,
-                              None,
-                              action_id=action.id)
+            action.set_status(self.READY)
+            dispatcher.notify(self.context, dispatcher.Dispatcher.NEW_ACTION,
+                              None, action_id=action.id)
 
         # Wait for cluster updating complete
-        # TODO(anyone): need db support
-        while self.get_status() != self.READY:
-            if scheduler.action_cancelled(self):
-                # During this period, if cancel request come,
-                # cancel this cluster updating, including all
-                # possible in-progress node update actions.
-                LOG.debug('Cluster updating action %s cancelled' % self.id)
-                self._cancel_update(cluster, old_profile_id)
-                return self.RES_CANCEL
-            elif scheduler.action_timeout(self):
-                # Action timeout, return
-                LOG.debug('Cluster updating action %s timeout' % self.id)
-                cluster.set_status(cluster.ERROR, 'cluster updating timeout')
-                db_api.cluster_lock_release(cluster.id, self.id)
-                return self.RES_TIMEOUT
+        result = self.RES_OK
+        if cluster.size > 0:
+            result = self._wait_for_action()
 
-            # Continue waiting (with sleep)
-            scheduler.reschedule(self, sleep=0)
+        if result == self.RES_OK:
+            cluster.set_status(cluster.ACTIVE, 'Cluster update completed')
 
-        # Cluster updating finished, set its status
-        # to active and release lock
-        cluster.set_status(cluster.ACTIVE, 'Cluster updating completed')
         db_api.cluster_lock_release(cluster.id, self.id)
 
         return self.RES_OK
 
-    def _cancel_update(self, cluster, old_profile_id):
-        node_list = cluster.get_nodes()
-        for node_id in node_list:
-            # We try to search node related action in DB
-            # TODO(anyone): we need a new db_api interface here
-            node_action = db_api.action_get_by_target(node_id)
-            if not node_action:
-                # Node action doesn't exist which means this
-                # action has been executed successfully, we
-                # don't do anything to it
-                continue
-            elif db_api.action_lock_check(self.context, node_action.id):
-                # If node action exist and is now in progress,
-                # try to cancel it.
-                scheduler.cancel_action(self.context, node_action.id)
-            else:
-                # Node action exist and not been locked,
-                # try to lock it and remove it from DB.
-
-                # Just use action_id as the owner_id when lock action
-                action = db_api.action_start_work_on(self.context,
-                                                     node_action.id,
-                                                     node_action.id)
-                if action:
-                    # Get lock successfully, delete this action
-                    db_api.action_delete(action.id)
-                else:
-                    # Action is locked by other worker, cancel it
-                    scheduler.cancel_action(self.context, action.id)
-
-            # Restore node obj
-            node = db_api.node_get(self.context, node_id)
-            node.do_update(self.context, profile_id=old_profile_id)
-
-        # We don't wait for node action cancel finishing
-        # TODO(anyone): may need more discussion
-
-        # Restore cluster based on old profile
-        res = cluster.do_update(self.context, profile_id=old_profile_id)
-
-        cluster.set_status(cluster.UPDATE_CANCELLED)
-        db_api.cluster_lock_release(cluster.id, self.id)
-
-        return res
-
     def do_delete(self, cluster):
-        # Set cluster status to DELETING
         cluster.set_status(cluster.DELETING)
-        # Try to lock the cluster
-        worker_id = db_api.cluster_lock_create(cluster.id, self.id)
-        if worker_id != self.id:
-            # Lock cluster failed, other action of this cluster
-            # is in progress, try to cancel it.
-            scheduler.cancel_action(self.context, worker_id)
+
+        # Try lock the cluster
+        action_id = db_api.cluster_lock_create(cluster.id, self.id)
+        if action_id != self.id:
+            # Lock failed, cancel the action that owns the lock
+            scheduler.cancel_action(self.context, action_id)
 
             # Sleep until this action get the lock or timeout
             while db_api.cluster_lock_create(cluster.id, self.id) != self.id:
                 if scheduler.action_timeout(self):
                     # Action timeout, set cluster status to ERROR and return
-                    LOG.debug('Cluster deleting action %s timeout' % self.id)
+                    LOG.debug('Cluster deletion %s timeout' % self.id)
                     cluster.set_status(cluster.ERROR,
-                                       'Cluster deletion timed out waiting')
+                                       'Cluster deletion timeout')
                     return self.RES_TIMEOUT
-                # Sleep for a while
+
                 scheduler.reschedule(self, sleep=0)
 
-        action_list = []
         node_list = cluster.get_nodes()
         for node_id in node_list:
             kwargs = {
-                'name': 'node-delete-%s' % node_id,
+                'name': 'node_delete_%s' % node_id[:8],
                 'context': self.context,
                 'target': node_id,
                 'cause': 'Cluster delete',
             }
             action = base.Action(self.context, 'NODE_DELETE', **kwargs)
-            action_list.append(action)
+            action.store(self.context)
+
+            # Build dependency and make the new action ready
+            db_api.action_add_dependency(action, self)
             action.set_status(self.READY)
 
-            # add new action to waiting/dependency list
-            db_api.action_add_dependency(action, self)
+            dispatcher.notify(self.context, dispatcher.Dispatcher.NEW_ACTION,
+                              None, action_id=action.id)
 
-        # Notify dispatcher
-        for action in action_list:
-            dispatcher.notify(self.context,
-                              dispatcher.Dispatcher.NEW_ACTION,
-                              None,
-                              action_id=action.id)
+        result = self.RES_OK
+        if cluster.size > 0:
+            result = self._wait_for_action()
 
-        # Wait for cluster creating complete
-        # TODO(anyone): need db supportting dependency based status management
-        while self.get_status() != self.READY:
-            if scheduler.action_cancelled(self):
-                # During this period, if cancel request come,
-                # cancel this cluster deleting immediately, then
-                # release the cluster lock and return.
-                LOG.debug('Cluster deleting action %s cancelled' % self.id)
-                cluster.set_status(cluster.ERROR,
-                                   'Cluster deleting cancelled')
-                db_api.cluster_lock_release(cluster.id, self.id)
-                return self.RES_CANCEL
-            elif scheduler.action_timeout(self):
-                # Action timeout, set cluster status to ERROR and return
-                LOG.debug('Cluster deleting action %s timed out' % self.id)
-                cluster.set_status(cluster.ERROR,
-                                   'Cluster deletion timed out')
-                db_api.cluster_lock_release(cluster.id, self.id)
-                return self.RES_TIMEOUT
+        if result == self.RES_OK:
+            cluster.do_delete(self.context)
 
-            # Continue waiting (with sleep)
-            scheduler.reschedule(self, sleep=0)
-
-        # Cluster is deleted successfully, set its
-        # status to DELETE and release the lock.
-        cluster.do_delete(self.context)
         db_api.cluster_lock_release(cluster.id, self.id)
 
         return self.RES_OK
@@ -321,27 +233,14 @@ class ClusterAction(base.Action):
     def do_scale_up(self, cluster):
         return self.RES_OK
 
-    def do_scale_down(self, cluster):
+    def do_scale_down(self, cluster, count, candidates):
+        # Go through all policies before scaling down.
         # Try to lock the cluster
         action_id = db_api.cluster_lock_create(cluster.id, self.id)
         if action_id != self.id:
-            # Lock cluster failed, other action of this cluster
-            # is in progress, give up this scaling down operation.
+            # Lock cluster failed, return with retry
             return self.RES_RETRY
 
-        # Go through all policies before scaling down.
-        policy_target = ('BEFORE', self.action)
-        result = self.policy_check(self.context, cluster.id, policy_target)
-        if not result:
-            # Policy check failed, release lock and return ERROR
-            db_api.cluster_lock_release(cluster.id, self.id)
-            return self.RES_ERROR
-
-        count = result.get('count', 0)
-        if count == 0:
-            return self.RES_ERROR
-
-        candidates = result.get('candidates', [])
         if len(candidates) == 0:
             # No candidates for scaling down op which means no DeletionPolicy
             # is attached to cluster, we just choose random nodes to
@@ -444,15 +343,24 @@ class ClusterAction(base.Action):
         return self.RES_OK
 
     def execute(self, **kwargs):
-        res = False
-        cluster = db_api.cluster_get(self.context, self.target)
+        res = self.RES_ERROR
+        cluster = clusterm.Cluster.load(self.context, self.target)
         if not cluster:
-            return self.RES_ERROR
+            LOG.error(_('Cluster %(name)s [%(id)s] not found') % {
+                'name': cluster.name, 'id': cluster.id})
+            return res
+
+        # do pre-action policy checking
+        check_result = self.policy_check(cluster.id, 'BEFORE')
+        if not check_result:
+            # Don't emit message here since policy_check should have done it
+            return res
 
         if self.action == self.CLUSTER_CREATE:
             res = self.do_create(cluster)
         elif self.action == self.CLUSTER_UPDATE:
             new_profile_id = self.inputs.get('new_profile_id')
+            # TODO(anyone): check null?
             res = self.do_update(cluster, new_profile_id)
         elif self.action == self.CLUSTER_DELETE:
             res = self.do_delete(cluster)
@@ -463,11 +371,21 @@ class ClusterAction(base.Action):
         elif self.action == self.CLUSTER_SCALE_UP:
             res = self.do_scale_up(cluster)
         elif self.action == self.CLUSTER_SCALE_DOWN:
-            res = self.do_scale_down(cluster)
+            count = check_result.get('count', 0)
+            if count == 0:
+                # We assume the policy has emit error/warnings
+                return self.RES_ERROR
+
+            candidates = check_result.get('candidates', [])
+            res = self.do_scale_down(cluster, count, candidates)
         elif self.action == self.CLUSTER_ATTACH_POLICY:
             res = self.do_attach_policy(cluster)
         elif self.action == self.CLUSTER_DETACH_POLICY:
             res = self.do_detach_policy(cluster)
+
+        if res == self.RES_OK:
+            check_result = self.policy_check(cluster.id, 'AFTER')
+            res = self.RES_OK if check_result else self.ERROR
 
         return res
 
