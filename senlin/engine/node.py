6cac2b1cb298a7cd39e359a59a635b9d8d7a87f2
tengqm <tengqim@cn.ibm.com>
2015-02-02 20:46:22 +0800

    Fixed node-delete and node-update path
    Node updated_time is supposed to be changed only when the profile or
    cluster_id is changed. Status change is not treated as a real 'update'
    operation.

--- senlin/engine/node.py
+++ senlin/engine/node.py
@@ -196,8 +196,9 @@ class Node(object):
         if status == self.ACTIVE and self.status == self.CREATING:
             values['created_time'] = now
         elif status == self.DELETED:
+            values['physical_id'] = ''
             values['deleted_time'] = now
-        else:
+        elif status == self.ACTIVE and self.status == self.UPDATING:
             values['updated_time'] = now
 
         self.status = status
@@ -225,16 +226,24 @@ class Node(object):
         return True
 
     def do_delete(self, context):
+        def _delete_db_record(node_id):
+            self.set_status(context, self.DELETED, reason='Deletion succeeded')
+            db_api.node_delete(context, self.id)
+            return
+
         if not self.physical_id:
+            _delete_db_record(self.id)
             return True
 
         # TODO(Qiming): check if actions are working on it and can be canceled
         # TODO(Qiming): log events
-        res = profile_base.delete_object(self)
+        self.set_status(context, self.DELETING, reason='Deletion in progress')
+        res = profile_base.Profile.delete_object(context, self)
         if res:
-            db_api.delete_node(self.id)
+            _delete_db_record(self.id)
             return True
         else:
+            self.set_status(context, self.ERROR, reason='Deletion failed')
             return False
 
     def do_update(self, context, new_profile_id):
