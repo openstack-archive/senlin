5d124474cc2230b77075588ed2333f5d2449b801
tengqm <tengqim@cn.ibm.com>
2015-02-03 17:39:58 +0800

    Added column 'scope' to cluster lock
    The scope field can be 'CLUSTER' or 'NODE'. The protocol for locking
    is like this:
    - A cluster lock, when created, must specify its scope.
    - A cluster-wide operation will specify 'CLUSTER' as its scope when
      creating a lock.
    - A node-level operation will try first creating a ClusterLock with
      'NODE' as its scope.
    - A request with 'CLUSTER' scope will fail when there is existing
      lock with whatever scope setting.
    - A request with 'NODE' scope will only succeed when either there
      is no lock or the existing lock scope is 'NODE'.

--- senlin/db/sqlalchemy/migrate_repo/versions/001_first_version.py
+++ senlin/db/sqlalchemy/migrate_repo/versions/001_first_version.py
@@ -94,6 +94,7 @@ def upgrade(migrate_engine):
                           sqlalchemy.ForeignKey('cluster.id'),
                           primary_key=True, nullable=False),
         sqlalchemy.Column('worker_id', sqlalchemy.String(36)),
+        sqlalchemy.Column('scope', sqlalchemy.String(32)),
         mysql_engine='InnoDB',
         mysql_charset='utf8'
     )
