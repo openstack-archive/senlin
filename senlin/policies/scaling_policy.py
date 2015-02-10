07fd343a65c258244c18a46b2fdf6bfbba43d1bf
tengqm <tengqim@cn.ibm.com>
2015-02-10 18:19:03 +0800

    Minor tweaking to scaling policy
    Nothing works yet.

--- senlin/policies/scaling_policy.py
+++ senlin/policies/scaling_policy.py
@@ -16,17 +16,17 @@ from senlin.policies import base
 
 
 class ScalingPolicy(base.Policy):
-    '''Policy for chaning the size of a cluster.
+    '''Policy for changing the size of a cluster.
 
-    This policy is expected to be enforced before the member list of a cluster
+    This policy is expected to be enforced before the node list of a cluster
     is changed.
     '''
 
     __type_name__ = 'ScalingPolicy'
 
     TARGET = [
-        ('BEFORE', consts.CLUSTER_SCALE_UP),
-        ('BEFORE', consts.CLUSTER_SCALE_DOWN),
+        ('BEFORE', consts.CLUSTER_SCALE_OUT),
+        ('BEFORE', consts.CLUSTER_SCALE_IN),
     ]
 
     PROFILE_TYPE = [
@@ -38,8 +38,15 @@ class ScalingPolicy(base.Policy):
 
         self.min_size = self.spec.get('min_size')
         self.max_size = self.spec.get('max_size')
-        self.adjustment_type = self.spec.get('adjustment_type')
-        self.adjustment_number = self.spec.get('adjustment_number')
+        adjustment = self.spec.get('adjustment')
+        if adjustment is not None:
+            self.adjustment_type = adjustment.get('type')
+            self.adjustment_number = adjustment.get('number')
+        else:
+            self.adjustment_type = self.DEFAULT_TYPE
+            self.adjustment_number = self.DEFAULT_NUMBER
+
+        self.min_step = self.spec.get('min_step')
 
         # TODO(anyone): Make sure the default cooldown can be used if
         # not specified
