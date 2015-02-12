78e4b2fe494ce988a1934f73bb0cc0eab80f716f
tengqm <tengqim@cn.ibm.com>
2015-02-12 23:44:59 +0800

    Initial implementation for scaling policy

--- senlin/policies/scaling_policy.py
+++ senlin/policies/scaling_policy.py
@@ -11,8 +11,13 @@
 # under the License.
 
 from senlin.common import consts
+from senlin.common.i18n import _
+from senlin.common.i18n import _LW
 from senlin.db import api as db_api
+from senlin.openstack.common import log as logging
 from senlin.policies import base
+
+LOG = logging.getLogger(__name__)
 
 
 class ScalingPolicy(base.Policy):
@@ -63,25 +68,39 @@ class ScalingPolicy(base.Policy):
         # not specified
 
     def pre_op(self, cluster_id, action, policy_data):
-        # TODO(anyone): get cluster size, calculate new size and return
-        # count of nodes need to create or delete;
-
-        # TODO(anyone): check if new size will break min_size or max_size
-        # constraints
-        policy_data['status'] = base.CHECK_OK
-        adjustment = self.adjustment_number
+        # TODO(Qiming): make this work
 
         nodes = db_api.node_get_all_by_cluster(action.context, cluster_id)
         current_size = len(nodes)
-        if current_size + adjustment > self.max_size:
-            adjustment = self.max_size - current_size
-        elif current_size + adjustment < self.min_size:
-            adjustment = current_size - self.min_size
 
-        pd = {'count': adjustment}
-        if action == consts.CLUSTER_SCALE_OUT:
-            policy_data['placement'] = pd
-        elif action == consts.CLUSTER_SCALE_IN:
+        if self.adjustment_type == self.EXACT_CAPACITY:
+            count = self.adjustment_number - current_size
+        elif self.adjustment_type == self.CHANGE_IN_CAPACITY:
+            count = self.adjustment_number
+        elif self.adjustment_type == self.CHANGE_IN_PERCENTAGE:
+            count = int((self.adjustment_number * current_size) / 100.0)
+            if count < self.adjustment_min_step:
+                count = self.adjustment_min_step
+        if current_size + count > self.max_size:
+            policy_data.status = base.CHECK_ERROR
+            policy_data.reason = _('Attempted scaling exceeds maximum size')
+        elif current_size + count < self.min_size:
+            policy_data.status = base.CHECK_ERROR
+            policy_data.reason = _('Attempted scaling exceeds minimum size')
+        else:
+            policy_data.status = base.CHECK_OK
+            policy_data.reason = _('Scaling request validated')
+
+        pd = {'count': count}
+        if action.action == consts.CLUSTER_SCALE_OUT:
+            if count < 0:
+                LOG.warning(_LW('Requesting a scale out operation but scaling '
+                                'policy generates a negative count.'))
+            policy_data['creation'] = pd
+        elif action.action == consts.CLUSTER_SCALE_IN:
+            if count > 0:
+                LOG.warning(_LW('Requesting a scale out operation but scaling '
+                                'policy generates a negative count.'))
             policy_data['deletion'] = pd
 
         return policy_data
