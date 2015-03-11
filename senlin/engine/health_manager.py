485ed38eac73b27265c33ed85382fcb4adeba063
tengqm <tengqim@cn.ibm.com>
2015-03-11 20:05:37 +0800

    Removed incorrect copyright claim
    Please be cautious when copy/paste files

--- senlin/engine/health_manager.py
+++ senlin/engine/health_manager.py
@@ -1,31 +1,28 @@
-# Copyright 2010 United States Government as represented by the
-# Administrator of the National Aeronautics and Space Administration.
-# All Rights Reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
 #
 #         http://www.apache.org/licenses/LICENSE-2.0
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
-"""Health Manager class.
+'''
+Health Manager class.
 
 Health Manager is responsible for monitoring the health of the clusters and
 take corresponding actions to recover the clusters based on the pre-defined
 health policies. 
+'''
 
-"""
-import oslo_messaging
 import random
 
 from oslo_config import cfg
 from oslo_log import log as logging
+import oslo_messaging
 
 from senlin.common import consts
 from senlin.common import messaging as rpc_messaging
