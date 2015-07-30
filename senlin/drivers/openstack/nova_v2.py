0dc3a3a47420e9b459839b3d4f04e4b3d4923dc7
tengqm <tengqim@cn.ibm.com>
2015-07-30 05:14:01 -0400

    Unit test cases for nova driver
    This patch refactors the nova v2 driver and adds unit test cases for the
    driver.
    
    Change-Id: Iced64d8e736fe418760cbfdc40a3faf5a02ab12b
    Closes-Bug: 1472853

--- senlin/drivers/openstack/nova_v2.py
+++ senlin/drivers/openstack/nova_v2.py
@@ -13,242 +13,144 @@
 import time
 
 from oslo_config import cfg
-from oslo_log import log
-from oslo_utils import timeutils
 
-from openstack.compute.v2 import server_metadata
-
-from senlin.common import exception
+from senlin.common.i18n import _
 from senlin.drivers import base
 from senlin.drivers.openstack import sdk
-
-LOG = log.getLogger(__name__)
 
 
 class NovaClient(base.DriverBase):
     '''Nova V2 driver.'''
 
-    def __init__(self, params):
-        self.conn = sdk.create_connection(params)
+    def __init__(self, ctx):
+        self.conn = sdk.create_connection(ctx)
         self.session = self.conn.session
 
-    def flavor_create(self, **params):
-        try:
-            return self.conn.compute.create_flavor(**params)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def flavor_create(self, **attrs):
+        return self.conn.compute.create_flavor(**attrs)
 
-    def flavor_get(self, flavor_id):
-        try:
-            return self.conn.compute.get_flavor(flavor_id)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def flavor_get(self, value):
+        return self.conn.compute.get_flavor(value)
 
-    def flavor_get_by_name(self, flavor_name):
-        try:
-            return self.conn.compute.find_flavor(flavor_name)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def flavor_get_by_name(self, name_or_id, ignore_missing=True):
+        return self.conn.compute.find_flavor(name_or_id, ignore_missing)
 
-    def flavor_list(self, details=False, **query):
-        try:
-            return self.conn.compute.flavors(details=details, **query)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def flavor_list(self, details=True, **query):
+        return self.conn.compute.flavors(details, **query)
 
-    def flavor_update(self, flavor_id, **params):
-        try:
-            return self.conn.compute.update_flavor(flavor_id, **params)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def flavor_update(self, value, **attrs):
+        return self.conn.compute.update_flavor(value, **attrs)
 
-    def flavor_delete(self, flavor_id, ignore_missing=True):
-        try:
-            self.conn.compute.delete_flavor(flavor_id,
-                                            ignore_missing=ignore_missing)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def flavor_delete(self, value, ignore_missing=True):
+        return self.conn.compute.delete_flavor(value, ignore_missing)
 
-    def image_create(self, **params):
-        raise NotImplemented
+    @sdk.translate_exception
+    def image_get(self, value):
+        return self.conn.compute.get_image(value)
 
-    def image_get(self, image_id):
-        try:
-            return self.conn.compute.get_image(image_id)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def image_get_by_name(self, name_or_id, ignore_missing=True):
+        return self.conn.compute.find_image(name_or_id, ignore_missing)
 
-    def image_get_by_name(self, image_name):
-        try:
-            return self.conn.compute.find_image(image_name,
-                                                ignore_missing=False)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def image_list(self, details=True, **query):
+        return self.conn.compute.images(details, **query)
 
-    def image_list(self, details=False, **query):
-        try:
-            return self.conn.compute.images(details=details, **query)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def image_delete(self, value, ignore_missing=True):
+        return self.conn.compute.delete_image(value, ignore_missing)
 
-    def image_delete(self, image_id, ignore_missing=True):
-        try:
-            return self.conn.compute.delete_image(
-                image_id, ignore_missing=ignore_missing)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def keypair_create(self, **attrs):
+        return self.conn.compute.create_keypair(**attrs)
 
-    def keypair_create(self, **params):
-        try:
-            return self.conn.compute.create_keypair(**params)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def keypair_get(self, value):
+        return self.conn.compute.get_keypair(value)
 
-    def keypair_get(self, keypair_id):
-        try:
-            return self.conn.compute.get_keypair(keypair_id)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def keypair_get_by_name(self, name_or_id, ignore_missing=True):
+        return self.conn.compute.find_keypair(name_or_id, ignore_missing)
 
-    def keypair_get_by_name(self, keypair_name):
-        try:
-            return self.conn.compute.find_keypair(keypair_name)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def keypair_list(self, **query):
+        return self.conn.compute.keypairs(**query)
 
-    def keypair_list(self, details=False, **query):
-        try:
-            return self.conn.compute.keypairs(details=details, **query)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def keypair_update(self, value, **attrs):
+        return self.conn.compute.update_keypair(value, **attrs)
 
-    def keypair_update(self, keypair_id, **params):
-        try:
-            return self.conn.compute.update_keypair(keypair_id, **params)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def keypair_delete(self, value, ignore_missing=True):
+        return self.conn.compute.delete_keypair(value, ignore_missing)
 
-    def keypair_delete(self, keypair_id, ignore_missing=True):
-        try:
-            self.conn.compute.delete_keypair(keypair_id,
-                                             ignore_missing=ignore_missing)
-        except sdk.exc.HttpException as ex:
-            raise ex
-
-    def server_create(self, **params):
+    @sdk.translate_exception
+    def server_create(self, **attrs):
         timeout = cfg.CONF.default_action_timeout
-        if 'timeout' in params:
-            timeout = params.pop('timeout')
+        if 'timeout' in attrs:
+            timeout = attrs.pop('timeout')
 
-        server_obj = None
-        try:
-            server_obj = self.conn.compute.create_server(**params)
-        except sdk.exc.HttpException as ex:
-            raise ex
-
-        try:
-            self.conn.compute.wait_for_server(server_obj, wait=timeout)
-        except sdk.exc.ResourceFailure as ex:
-            raise exception.ProfileOperationFailed(ex.message)
-        except sdk.exc.ResourceTimeout as ex:
-            raise exception.ProfileOperationTimeout(ex.message)
+        server_obj = self.conn.compute.create_server(**attrs)
+        self.conn.compute.wait_for_server(server_obj, wait=timeout)
 
         return server_obj
 
-    def server_get(self, server_id):
-        try:
-            return self.conn.compute.get_server(server_id)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def server_get(self, value):
+        return self.conn.compute.get_server(value)
 
-    def server_list(self, details=False, **query):
-        try:
-            return self.conn.compute.servers(details=details, **query)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def server_list(self, details=True, **query):
+        return self.conn.compute.servers(details, **query)
 
-    def server_update(self, server_id, **params):
-        try:
-            return self.conn.compute.update_server(server_id, **params)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def server_update(self, value, **attrs):
+        return self.conn.compute.update_server(value, **attrs)
 
-    def server_delete(self, server_id, ignore_missing=True):
+    @sdk.translate_exception
+    def server_delete(self, value, ignore_missing=True):
+        # TODO(Qiming): Need to find a better way to handle not found
+        #               exception, maybe the wait is not necessary
         timeout = cfg.CONF.default_action_timeout
 
-        try:
-            self.conn.compute.delete_server(server_id,
-                                            ignore_missing=ignore_missing)
-        except sdk.exc.HttpException as ex:
-            raise ex
-
-        stopwatch = timeutils.StopWatch(timeout)
-        stopwatch.start()
-        while not stopwatch.expired():
+        self.conn.compute.delete_server(value, ignore_missing)
+        sleep_count = 0
+        while sleep_count < timeout:
             try:
-                self.server_get(server_id)
-            except Exception as ex:
-                sdk.ignore_not_found(ex)
+                self.server_get(value)
+            except sdk.exc.NotFoundException:
                 return
             time.sleep(1)
+            sleep_count += 1
 
-        raise exception.ProfileOperationTimeout(ex.message)
+        raise Exception(_('Server deletion timeout.'))
 
-    def server_interface_create(self, **params):
-        try:
-            return self.conn.compute.create_server_interface(**params)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def server_interface_create(self, **attrs):
+        return self.conn.compute.create_server_interface(**attrs)
 
-    def server_interface_get(self, interface_id):
-        try:
-            return self.conn.compute.get_server_interface(interface_id)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def server_interface_get(self, value):
+        return self.conn.compute.get_server_interface(value)
 
-    def server_interface_list(self):
-        try:
-            return self.conn.compute.server_interfaces()
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def server_interface_list(self, **query):
+        return self.conn.compute.server_interfaces(**query)
 
-    def server_interface_update(self, interface_id, **params):
-        try:
-            return self.conn.compute.update_server_interface(interface_id,
-                                                             **params)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def server_interface_update(self, value, **attrs):
+        return self.conn.compute.update_server_interface(value, **attrs)
 
-    def server_interface_delete(self, interface_id, ignore_missing=True):
-        try:
-            self.conn.compute.delete_server_interface(
-                interface_id, ignore_missing=ignore_missing)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def server_interface_delete(self, value, ignore_missing=True):
+        return self.conn.compute.delete_server_interface(value, ignore_missing)
 
-    def server_ip_list(self, **params):
-        try:
-            return self.conn.compute.server_ips()
-        except sdk.exc.HttpException as ex:
-            raise ex
-
-    def server_metadata_create(self, **params):
-        obj = server_metadata.ServerMetadata.new(**params)
-        try:
-            return obj.create(self.session)
-        except sdk.exc.HttpException as ex:
-            raise ex
-
-    def server_metadata_get(self, **params):
-        obj = server_metadata.ServerMetadata.new(**params)
-        try:
-            return obj.get(self.session)
-        except sdk.exc.HttpException as ex:
-            raise ex
-
-    def server_metadata_update(self, **params):
-        obj = server_metadata.ServerMetadata.new(**params)
-        try:
-            return obj.update(self.session)
-        except sdk.exc.HttpException as ex:
-            raise ex
+    @sdk.translate_exception
+    def server_ip_list(self, **query):
+        return self.conn.compute.server_ips(**query)
