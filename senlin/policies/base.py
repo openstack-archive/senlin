fc60900793b4bb8d05d3c39e4e397ae7287a63a8
tengqm <tengqim@cn.ibm.com>
2015-08-08 07:00:07 -0400

    Revise policy enforcement level implementation
    This patch moves the enforcement level definition to the policy base
    module. It makes the SHOULD level as the default level for policies when
    not specified. This patch also adds test cases for a utility function.
    
    Change-Id: I80a4511e939e46d7fd6c9ff6c168fe27a3dc57e0

--- senlin/policies/base.py
+++ senlin/policies/base.py
@@ -18,11 +18,62 @@ from senlin.common import schema
 from senlin.db import api as db_api
 from senlin.engine import environment
 
+# Default enforcement levels and level names.
+#
+#  - MUST: A policy of this enforcement level must be strictly checked. A
+#          violation of such a policy will lead a cluster to ``CRITICAL``
+#          status, which means that the cluster is in a problematic status
+#          that cannot be recovered using Senlin APIs. A manual intervention
+#          is needed. Such a cluster should not be treated as funtional any
+#          more.
+#
+#  - SHOULD: A violation of a policy at this enforcement level will render a
+#            cluster into an ``ERROR`` status. A manual intervention is needed
+#            to recover the cluster. The cluster may and may not be providing
+#            services in the ``ERROR`` status.
+#
+#  - WOULD: A policy of this enforcement level is usually about some
+#           operations that would be done when the policy is enforced. A
+#           violation of the policy will leave the cluster in a ``WARNING``
+#           status, which means that the cluster is still operational, but
+#           there are unsuccessful operations attempted.
+#
+#  - MIGHT: A policy of this enforcement level is usually associated with
+#           certain operations that may or may not be done. A violation of
+#           this policy will not cause any negative impact to the cluster.
+#
+
+POLICY_LEVELS = (
+    MUST, SHOULD, WOULD, MIGHT,
+) = (
+    50, 40, 30, 20
+)
+
+_levelNames = {
+    MUST: 'MUST',
+    SHOULD: 'SHOULD',
+    WOULD: 'WOULD',
+    MIGHT: 'MIGHT',
+    'MUST': MUST,
+    'SHOULD': SHOULD,
+    'WOULD': WOULD,
+    'MIGHT': MIGHT,
+}
+
 CHECK_RESULTS = (
     CHECK_OK, CHECK_ERROR
 ) = (
     'OK', 'ERROR'
 )
+
+
+def getLevelName(level):
+    '''Get a level name or number.
+
+    Return a level name if given a numeric value; or return a value if given
+    a string.  If level is not predefined, "Level %s" will be returned.
+    '''
+    return _levelNames.get(level, ("Level %s" % level))
 
 
 class Policy(object):
