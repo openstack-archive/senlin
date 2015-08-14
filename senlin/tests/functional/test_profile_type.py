eba23f8dc96e9f14a27b813313f579b6b9146f09
yanyanhu <yanyanhu@cn.ibm.com>
2015-08-14 04:58:23 -0400

    Add functional test for listing profile_type
    This patch adds a functional test case for listing Senlin profile
    types.
    
    Change-Id: I2510e3d0d7d3b9a216019dc1fda6aebe4a25e2fd

---
+++ senlin/tests/functional/test_profile_type.py
@@ -0,0 +1,27 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from senlin.tests.functional import base
+
+
+class TestProfileType(base.SenlinFunctionalTest):
+
+    def test_get_profile_types(self):
+        # Check that listing profile types works.
+        rel_url = 'profile_types'
+        status = [200]
+        resp = self.client.api_request('GET', rel_url,
+                                       expected_resp_status=status)
+        profile_types = resp.body['profile_types']
+        expected_profile_types = [{'name': 'os.nova.server'},
+                                  {'name': 'os.heat.stack'}]
+        self.assertEqual(expected_profile_types, profile_types)
