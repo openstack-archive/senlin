542e2fb72b4a9435b13cd6214121235cc07381c0
tengqm <tengqim@cn.ibm.com>
2016-01-18 00:47:55 -0500

    Add version controller for V1 API
    This patch adds a version resource controller for the senlin V1 API.
    It is based on a guideline from api-wg (under review):
    
    https://review.openstack.org/#/c/254895
    
    Change-Id: I2c6d83ba60f0350ed4a65b6bfb54e562284b486a

---
+++ senlin/api/openstack/v1/version.py
@@ -0,0 +1,39 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+class VersionController(object):
+    """WSGI controller for version in Senlin v1 API."""
+
+    def __init__(self, conf):
+        self.conf = conf
+
+    def version(self, req):
+        version_info = {
+            'id': '1.0',
+            'status': 'CURRENT',
+            'updated': '2016-01-18T00:00:00Z',
+            'media-types': [
+                {
+                    'base': 'application/json',
+                    'type': 'application/vnd.openstack.clustering-v1+json'
+                }
+            ],
+            'links': [
+                {
+                    'href': '.',
+                    'rel': 'self'
+                }
+            ]
+        }
+
+        return {'version': version_info}
