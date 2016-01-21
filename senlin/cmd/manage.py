4d44d58c91e6a4fdf9ab16acac6320dd5d1f6bb9
Ethan Lynn <xjunlin@cn.ibm.com>
2016-01-21 17:13:20 +0800

    Add senlin-manage service list/clean for engine status
    Add senlin-manage service list/clean for engine status
    
    blueprint senlin-manage-service-list
    Change-Id: Ic51040352ecb149b9c1c2328dad62312829562b8

--- senlin/cmd/manage.py
+++ senlin/cmd/manage.py
@@ -18,7 +18,9 @@ import sys
 
 from oslo_config import cfg
 from oslo_log import log as logging
+from oslo_utils import timeutils
 
+from senlin.common.i18n import _
 from senlin.db import api
 from senlin import version
 
@@ -38,12 +40,74 @@ def do_db_sync():
     api.db_sync(api.get_engine(), CONF.command.version)
 
 
+class ServiceManageCommand(object):
+    def _format_service(self, service):
+        if service is None:
+            return
+
+        status = 'down'
+        if ((timeutils.utcnow() - service.updated_at).total_seconds() <=
+                CONF.periodic_interval):
+            status = 'up'
+
+        result = {
+            'service_id': service.id,
+            'binary': service.binary,
+            'host': service.host,
+            'topic': service.topic,
+            'created_at': service.created_at,
+            'updated_at': service.updated_at,
+            'status': status
+        }
+        return result
+
+    def service_list(self):
+        services = [self._format_service(service)
+                    for service in api.service_get_all()]
+
+        print_format = "%-36s %-24s %-16s %-16s %-10s %-24s %-24s"
+        print(print_format % (_('Service ID'),
+                              _('Host'),
+                              _('Binary'),
+                              _('Topic'),
+                              _('Status'),
+                              _('Created At'),
+                              _('Updated At')))
+
+        for svc in services:
+            print(print_format % (svc['service_id'],
+                                  svc['host'],
+                                  svc['binary'],
+                                  svc['topic'],
+                                  svc['status'],
+                                  svc['created_at'],
+                                  svc['updated_at']))
+
+    def service_clean(self):
+        for service in api.service_get_all():
+            svc = self._format_service(service)
+            if svc['status'] == 'down':
+                print(_('Dead service %s is removed.') % svc['service_id'])
+                api.service_delete(svc['service_id'])
+
+    @staticmethod
+    def add_service_parsers(subparsers):
+        service_parser = subparsers.add_parser('service')
+        service_parser.set_defaults(command_object=ServiceManageCommand)
+        service_subparsers = service_parser.add_subparsers(dest='action')
+        list_parser = service_subparsers.add_parser('list')
+        list_parser.set_defaults(func=ServiceManageCommand().service_list)
+        remove_parser = service_subparsers.add_parser('clean')
+        remove_parser.set_defaults(func=ServiceManageCommand().service_clean)
+
+
 def add_command_parsers(subparsers):
     parser = subparsers.add_parser('db_version')
     parser.set_defaults(func=do_db_version)
 
     parser = subparsers.add_parser('db_sync')
     parser.set_defaults(func=do_db_sync)
+    ServiceManageCommand.add_service_parsers(subparsers)
     parser.add_argument('version', nargs='?')
     parser.add_argument('current_version', nargs='?')
 
