4a244a3b8e73d5e5d59dda7d6932e69e652e9157
Ethan Lynn <xjunlin@cn.ibm.com>
2016-04-02 14:04:37 +0800

    Remove concurrency constraint for functional tests
    Remove concurrency constraint for functional tests, and fix
    the issues found after enable concurrency.
    
    Change-Id: I7d27009ed9f7cec9f28c727d836a0efc1c75ef41
    (cherry picked from commit 5866a182ccd5c5f572acb4aa893c04176058fdd2)

--- senlin/tests/functional/test_cluster_membership.py
+++ senlin/tests/functional/test_cluster_membership.py
@@ -76,8 +76,7 @@ class TestClusterMembership(base.SenlinFunctionalTest):
         node2 = test_api.get_node(self.client, node2['id'])
         self.assertEqual(cluster['id'], node1['cluster_id'])
         self.assertEqual(cluster['id'], node2['cluster_id'])
-        self.assertEqual(2, node1['index'])
-        self.assertEqual(3, node2['index'])
+        self.assertEqual([2, 3], sorted([node1['index'], node2['index']]))
 
         # Remove nodes from cluster
         params = {
