a7e43fc153fbd9a00d203bce00d8180c27190a18
yanyanhu <yanyanhu@cn.ibm.com>
2016-05-10 05:02:32 -0400

    Add tempest API test for cluster show/list
    This patch adds tempest API test for cluster show/list.
    
    Change-Id: Ibe1b4619c17816f52349740144cdabd69fc894fc

---
+++ senlin/tests/tempest/api/clusters/test_cluster_list.py
@@ -0,0 +1,56 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from tempest.lib import decorators
+
+from senlin.tests.tempest.api import base
+from senlin.tests.tempest.common import constants
+
+
+class TestClusterList(base.BaseSenlinTest):
+
+    @classmethod
+    def resource_setup(cls):
+        super(TestClusterList, cls).resource_setup()
+        # Create profile
+        cls.profile = cls.create_profile(
+            constants.spec_nova_server)
+        # Create a test cluster
+        cls.cluster = cls.create_test_cluster(cls.profile['id'], 0)
+
+    @classmethod
+    def resource_cleanup(cls):
+        # Delete test cluster
+        cls.delete_test_cluster(cls.cluster['id'])
+        # Delete profile
+        cls.delete_profile(cls.profile['id'])
+        super(TestClusterList, cls).resource_cleanup()
+
+    @decorators.idempotent_id('943cdc02-81bd-4200-a08d-bc1932d932f7')
+    def test_cluster_list(self):
+        res = self.client.list_objs('clusters')
+
+        # Verify resp of cluster list API
+        self.assertEqual(200, res['status'])
+        self.assertIsNone(res['location'])
+        self.assertIsNotNone(res['body'])
+        clusters = res['body']
+        cluster_ids = []
+        for cluster in clusters:
+            for key in ['created_at', 'data', 'desired_capacity', 'domain',
+                        'id', 'init_at', 'max_size', 'metadata', 'min_size',
+                        'name', 'nodes', 'policies', 'profile_id',
+                        'profile_name', 'project', 'status', 'status_reason',
+                        'timeout', 'updated_at', 'user']:
+                self.assertIn(key, cluster)
+            cluster_ids.append(cluster['id'])
+        self.assertIn(self.cluster['id'], cluster_ids)
