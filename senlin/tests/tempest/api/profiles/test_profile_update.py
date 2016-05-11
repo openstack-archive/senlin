f9815ac660263f04463a1d15e8c0a87edca518ca
Ethan Lynn <xjunlin@cn.ibm.com>
2016-05-11 22:27:42 +0800

    Add API test for profile update
    Add API test for profile update
    
    Change-Id: I82a15c72409013c67a1f99fadeea45c52977588d

---
+++ senlin/tests/tempest/api/profiles/test_profile_update.py
@@ -0,0 +1,50 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from tempest.lib import decorators
+
+from senlin.tests.tempest.api import base
+from senlin.tests.tempest.common import constants
+
+
+class TestProfileUpdate(base.BaseSenlinTest):
+
+    @classmethod
+    def resource_setup(cls):
+        super(TestProfileUpdate, cls).resource_setup()
+        # Create profile
+        cls.profile = cls.create_profile(constants.spec_nova_server)
+
+    @classmethod
+    def resource_cleanup(cls):
+        # Delete profile
+        cls.client.delete_obj('profiles', cls.profile['id'])
+
+    @decorators.idempotent_id('d7efdd92-1687-444e-afcc-b7f9c7e37478')
+    def test_update_profile(self):
+        params = {
+            'profile': {
+                'name': 'updated-profile-name',
+                'metadata': {'bar': 'foo'}
+            }
+        }
+        res = self.client.update_obj('profiles', self.profile['id'], params)
+
+        # Verify resp of profile update API
+        self.assertEqual(200, res['status'])
+        self.assertIsNotNone(res['body'])
+        profile = res['body']
+        for key in ['created_at', 'domain', 'id', 'metadata', 'name',
+                    'project', 'spec', 'type', 'updated_at', 'user']:
+            self.assertIn(key, profile)
+        self.assertEqual('updated-profile-name', profile['name'])
+        self.assertEqual({'bar': 'foo'}, profile['metadata'])
