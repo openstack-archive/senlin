aec21cb262f860238c66df497c7a558da405a9e3
Ethan Lynn <xjunlin@cn.ibm.com>
2016-05-11 22:14:38 +0800

    Add API tests for profile create
    Add API tests for profile create
    
    Change-Id: I0040be4f599b0adf245699995f3257659a1f366a

---
+++ senlin/tests/tempest/api/profiles/test_profile_create.py
@@ -0,0 +1,55 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from tempest.lib import decorators
+
+from senlin.tests.tempest.api import base
+from senlin.tests.tempest.common import constants
+
+
+class TestProfileCreate(base.BaseSenlinTest):
+
+    @classmethod
+    def resource_setup(cls):
+        super(TestProfileCreate, cls).resource_setup()
+        cls.profiles = []
+
+    @classmethod
+    def resource_cleanup(cls):
+        # Delete profile
+        for profile in cls.profiles:
+            cls.client.delete_obj('profiles', profile)
+        super(TestProfileCreate, cls).resource_cleanup()
+
+    @decorators.idempotent_id('76216581-e78a-42f5-bf1d-65d83bd206fc')
+    def test_create_profile(self):
+        params = {
+            'profile': {
+                'name': 'test-profile',
+                'spec': constants.spec_nova_server,
+                'metadata': {'foo': 'bar'}
+            }
+        }
+        res = self.client.create_obj('profiles', params)
+
+        # Verify resp of profile create API
+        self.assertEqual(201, res['status'])
+        self.assertIsNotNone(res['body'])
+        profile = res['body']
+        self.profiles.append(profile['id'])
+        for key in ['created_at', 'domain', 'id', 'metadata', 'name',
+                    'project', 'spec', 'type', 'updated_at', 'user']:
+            self.assertIn(key, profile)
+        self.assertEqual('test-profile', profile['name'])
+        self.assertEqual('os.nova.server-1.0', profile['type'])
+        self.assertEqual(constants.spec_nova_server, profile['spec'])
+        self.assertEqual({'foo': 'bar'}, profile['metadata'])
