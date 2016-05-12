e4e7419dc6f1e19d4d58f382c6b7b1157d2a4dd5
xu-haiwei <hai-xu@xr.jp.nec.com>
2016-05-12 14:17:22 +0900

    Add support for container profile
    This patch creates a new container type profile, currently
    only support docker.
    
    blueprint container-profile-support
    
    Change-Id: I7ff2c77f1e0fd06f26bb46c80d17c616f361e1bc

---
+++ senlin/tests/unit/profiles/test_container_docker.py
@@ -0,0 +1,39 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from senlin.profiles.container import docker
+from senlin.tests.unit.common import base
+from senlin.tests.unit.common import utils
+
+
+class TestContainerDockerProfile(base.SenlinTestCase):
+
+    def setUp(self):
+        super(TestContainerDockerProfile, self).setUp()
+
+        self.context = utils.dummy_context()
+        self.spec = {
+            'type': 'container.docker',
+            'version': '1.0',
+            'properties': {
+                'context': {
+                    'region_name': 'RegionOne'
+                },
+                'name': 'docker_container',
+                'image': 'hello-world',
+                'command': '/bin/sleep 30',
+            }
+        }
+
+    def test_init(self):
+        profile = docker.DockerProfile('t', self.spec)
+        self.assertIsNone(profile._dockerclient)
