6662235e4d75b5f05e0cc6c4ad90ac186e5faa80
tengqm <tengqim@cn.ibm.com>
2016-05-25 05:45:09 -0400

    versioned object -- receiver
    Change-Id: Ib5202f824dc06f955884ddbe6f07e852e218aaa1

---
+++ senlin/objects/receiver.py
@@ -0,0 +1,75 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Receiver object."""
+
+from oslo_versionedobjects import base
+from oslo_versionedobjects import fields
+
+from senlin.db import api as db_api
+from senlin.objects import base as senlin_base
+
+
+class Receiver(senlin_base.SenlinObject, base.VersionedObjectDictCompat):
+    """Senlin receiver object."""
+
+    fields = {
+        'id': fields.UUIDField(),
+        'name': fields.StringField(),
+        'type': fields.StringField(),
+        'cluster_id': fields.UUIDField(),
+        'actor': fields.DictOfStringField(nullable=True),
+        'action': fields.StringField(),
+        'params': fields.DictOfStringField(nullable=True),
+        'channel': fields.DictOfStringField(nullable=True),
+        'user': fields.StringField(),
+        'project': fields.StringField(),
+        'domain': fields.StringField(),
+    }
+
+    @staticmethod
+    def _from_db_object(context, receiver, db_obj):
+        for field in receiver.fields:
+            receiver[field] = db_obj[field]
+
+        receiver._context = context
+        receiver.obj_reset_changes()
+
+        return receiver
+
+    @classmethod
+    def create(cls, context, values):
+        obj = db_api.receiver_create(context, values)
+        return cls._from_db_object(context, cls(context), obj)
+
+    @classmethod
+    def get(cls, context, receiver_id):
+        obj = db_api.receiver_get(context, receiver_id)
+        return cls._from_db_object(context, cls(), obj)
+
+    @classmethod
+    def get_by_name(cls, context, name, **kwargs):
+        obj = db_api.receiver_get_by_name(context, name, **kwargs)
+        return cls._from_db_object(context, cls(), obj)
+
+    @classmethod
+    def get_by_short_id(cls, context, short_id, **kwargs):
+        obj = db_api.receiver_get_by_short_id(context, short_id, **kwargs)
+        return cls._from_db_object(context, cls(), obj)
+
+    @classmethod
+    def get_all(cls, context, **kwargs):
+        return db_api.receiver_get_all(context, **kwargs)
+
+    @classmethod
+    def delete(cls, context, receiver_id):
+        db_api.receiver_delete(context, receiver_id)
