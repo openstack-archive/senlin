b208ed17e3340416fc9dc0bab227da64d89b30d4
tengqm <tengqim@cn.ibm.com>
2016-05-25 05:00:54 -0400

    versioned objects -- cluster lock and node lock
    Change-Id: Ida604aac38c8876068f5d769539497a4cb638d4d

---
+++ senlin/objects/cluster_lock.py
@@ -0,0 +1,41 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Cluster lock object."""
+
+from oslo_versionedobjects import base
+from oslo_versionedobjects import fields
+
+from senlin.db import api as db_api
+from senlin.objects import base as senlin_base
+
+
+class ClusterLock(senlin_base.SenlinObject, base.VersionedObjectDictCompat):
+    """Senlin cluster lock object."""
+
+    fields = {
+        'cluster_id': fields.UUIDField(),
+        'action_ids': fields.ListOfStringsField(),
+        'semaphore': fields.IntegerField(),
+    }
+
+    @classmethod
+    def acquire(cls, cluster_id, action_id, scope):
+        return db_api.cluster_lock_acquire(cluster_id, action_id, scope)
+
+    @classmethod
+    def release(cls, cluster_id, action_id, scope):
+        return db_api.cluster_lock_release(cluster_id, action_id, scope)
+
+    @classmethod
+    def steal(cls, cluster_id, action_id):
+        return db_api.cluster_lock_steal(cluster_id, action_id)
---
+++ senlin/objects/node_lock.py
@@ -0,0 +1,40 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Node lock object."""
+
+from oslo_versionedobjects import base
+from oslo_versionedobjects import fields
+
+from senlin.db import api as db_api
+from senlin.objects import base as senlin_base
+
+
+class NodeLock(senlin_base.SenlinObject, base.VersionedObjectDictCompat):
+    """Senlin node lock object."""
+
+    fields = {
+        'node_id': fields.UUIDField(),
+        'action_id': fields.UUIDField(),
+    }
+
+    @classmethod
+    def acquire(cls, node_id, action_id):
+        return db_api.node_lock_acquire(node_id, action_id)
+
+    @classmethod
+    def release(cls, node_id, action_id):
+        return db_api.node_lock_release(node_id, action_id)
+
+    @classmethod
+    def steal(cls, node_id, action_id):
+        return db_api.node_lock_steal(node_id, action_id)
