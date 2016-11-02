9482e604b3b984c195267778d73be09867d87227
tengqm <tengqim@cn.ibm.com>
2016-11-02 03:28:54 -0400

    Request object for policy update and detach
    This patch adds versioned objects for cluster-policy-update and
    cluster-policy-detach requests.
    
    Change-Id: I6d987e54e5c95f01f6dadd920b0034f0ef268b16

--- senlin/tests/unit/objects/requests/test_clusters.py
+++ senlin/tests/unit/objects/requests/test_clusters.py
@@ -424,3 +424,34 @@ class TestClusterAttachPolicy(test_base.SenlinTestCase):
                                identity='foo', enabled='Bogus')
 
         self.assertIn("Unrecognized value 'Bogus'", six.text_type(ex))
+
+
+class TestClusterUpdatePolicy(test_base.SenlinTestCase):
+
+    def test_init(self):
+        sot = clusters.ClusterUpdatePolicyRequest(identity='foo',
+                                                  policy_id='bar')
+
+        self.assertEqual('foo', sot.identity)
+        self.assertEqual('bar', sot.policy_id)
+        self.assertFalse(sot.obj_attr_is_set('enabled'))
+
+        sot.obj_set_defaults()
+        self.assertTrue(sot.obj_attr_is_set('enabled'))
+        self.assertEqual(True, sot.enabled)
+
+    def test_init_failed(self):
+        ex = self.assertRaises(ValueError,
+                               clusters.ClusterUpdatePolicyRequest,
+                               identity='foo', enabled='Bogus')
+
+        self.assertIn("Unrecognized value 'Bogus'", six.text_type(ex))
+
+
+class TestClusterDetachPolicy(test_base.SenlinTestCase):
+
+    def test_init(self):
+        sot = clusters.ClusterDetachPolicyRequest(identity='foo',
+                                                  policy_id='bar')
+        self.assertEqual('foo', sot.identity)
+        self.assertEqual('bar', sot.policy_id)
