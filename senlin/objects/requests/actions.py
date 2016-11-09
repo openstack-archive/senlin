60a002bb63429c9602edf2d7ad8ceade3a5b235f
jonnary <liu.xuefeng1@zte.com.cn>
2016-11-09 09:56:34 +0800

    Add request object for action-list
    This patch adds request object for action-list
    
    Change-Id: I71953f3e57f5a8d62dae69551859ebfb511c7a8f

---
+++ senlin/objects/requests/actions.py
@@ -0,0 +1,36 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from senlin.common import consts
+from senlin.objects import base
+from senlin.objects import fields
+
+
+@base.SenlinObjectRegistry.register
+class ActionListRequest(base.SenlinObject):
+
+    action_name_list = list(consts.CLUSTER_ACTION_NAMES)
+    action_name_list.extend(list(consts.NODE_ACTION_NAMES))
+
+    fields = {
+        'name': fields.ListOfStringsField(nullable=True),
+        'action': fields.ListOfEnumField(
+            valid_values=action_name_list, nullable=True),
+        'target': fields.ListOfStringsField(nullable=True),
+        'status': fields.ListOfEnumField(
+            valid_values=list(consts.ACTION_STATUSES), nullable=True),
+        'limit': fields.NonNegativeIntegerField(nullable=True),
+        'marker': fields.UUIDField(nullable=True),
+        'sort': fields.SortField(
+            valid_keys=list(consts.ACTION_SORT_KEYS), nullable=True),
+        'project_safe': fields.FlexibleBooleanField(default=True)
+    }
---
+++ senlin/tests/unit/objects/requests/test_actions.py
@@ -0,0 +1,43 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from senlin.objects.requests import actions
+from senlin.tests.unit.common import base as test_base
+
+
+class TestActionList(test_base.SenlinTestCase):
+
+    def test_action_list_request_body_full(self):
+        params = {
+            'name': ['node_create_12345678'],
+            'action': ['NODE_CREATE'],
+            'target': ['0df0931b-e251-4f2e-8719-4effda3627bb'],
+            'status': ['READY'],
+            'limit': 5,
+            'marker': 'f1ed0d50-7651-4599-a8cb-c86e9c7123f6',
+            'sort': 'name:asc',
+            'project_safe': False,
+        }
+        sot = actions.ActionListRequest(**params)
+        self.assertEqual(['node_create_12345678'], sot.name)
+        self.assertEqual(['NODE_CREATE'], sot.action)
+        self.assertEqual(['0df0931b-e251-4f2e-8719-4effda3627bb'], sot.target)
+        self.assertEqual(['READY'], sot.status)
+        self.assertEqual(5, sot.limit)
+        self.assertEqual('f1ed0d50-7651-4599-a8cb-c86e9c7123f6', sot.marker)
+        self.assertEqual('name:asc', sot.sort)
+        self.assertFalse(sot.project_safe)
+
+    def test_action_list_request_body_default(self):
+        sot = actions.ActionListRequest()
+        sot.obj_set_defaults()
+        self.assertTrue(sot.project_safe)
