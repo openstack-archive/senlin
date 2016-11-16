fcbb4e3e955c6e20c7976abe80119aa90d2b9240
jonnary <liu.xuefeng1@zte.com.cn>
2016-11-16 20:13:22 +0800

    Add engine support for event_list2
    This patch adds engine support for event_list2.
    
    Change-Id: Ic857ce81fac85606331e71b27ead7510d4fb18da

--- senlin/tests/unit/engine/service/test_events.py
+++ senlin/tests/unit/engine/service/test_events.py
@@ -16,9 +16,11 @@ from oslo_messaging.rpc import dispatcher as rpc
 from oslo_utils import uuidutils
 import six
 
+from senlin.common import consts
 from senlin.common import exception as exc
 from senlin.engine import service
 from senlin.objects import event as eo
+from senlin.objects.requests import events as oreo
 from senlin.tests.unit.common import base
 from senlin.tests.unit.common import utils
 
@@ -188,6 +190,79 @@ class EventTest(base.SenlinTestCase):
         mock_load.assert_called_once_with(self.ctx, filters=None, sort=None,
                                           limit=None, marker=None,
                                           project_safe=True)
+
+    @mock.patch.object(eo.Event, 'get_all')
+    def test_event_list2(self, mock_load):
+        obj_1 = mock.Mock()
+        obj_1.as_dict.return_value = {'level': consts.EVENT_LEVELS['DEBUG']}
+        obj_2 = mock.Mock()
+        obj_2.as_dict.return_value = {'level': consts.EVENT_LEVELS['INFO']}
+
+        mock_load.return_value = [obj_1, obj_2]
+
+        req = oreo.EventListRequest()
+        result = self.eng.event_list2(self.ctx, req.obj_to_primitive())
+        expected = [{'level': 'DEBUG'}, {'level': 'INFO'}]
+
+        self.assertEqual(expected, result)
+        mock_load.assert_called_once_with(self.ctx, project_safe=True)
+
+    @mock.patch.object(eo.Event, 'get_all')
+    def test_event_list2_with_params(self, mock_load):
+        obj_1 = mock.Mock()
+        obj_1.as_dict.return_value = {'level': consts.EVENT_LEVELS['DEBUG']}
+        obj_2 = mock.Mock()
+        obj_2.as_dict.return_value = {'level': consts.EVENT_LEVELS['INFO']}
+
+        mock_load.return_value = [obj_1, obj_2]
+
+        marker_uuid = '8216a86c-1bdc-442e-b493-329385d37cbc'
+        req = oreo.EventListRequest(level=['DEBUG', 'INFO'],
+                                    limit=123,
+                                    marker=marker_uuid,
+                                    sort=consts.EVENT_TIMESTAMP,
+                                    project_safe=True)
+        result = self.eng.event_list2(self.ctx, req.obj_to_primitive())
+        expected = [{'level': 'DEBUG'}, {'level': 'INFO'}]
+        self.assertEqual(expected, result)
+
+        filters = {'level': [consts.EVENT_LEVELS['DEBUG'],
+                             consts.EVENT_LEVELS['INFO']]}
+        mock_load.assert_called_once_with(self.ctx, filters=filters,
+                                          sort=consts.EVENT_TIMESTAMP,
+                                          limit=123,
+                                          marker=marker_uuid,
+                                          project_safe=True)
+
+    def test_event_list2_with_bad_params(self):
+        req = oreo.EventListRequest(project_safe=False)
+        ex = self.assertRaises(rpc.ExpectedException,
+                               self.eng.event_list2,
+                               self.ctx, req.obj_to_primitive())
+        self.assertEqual(exc.Forbidden, ex.exc_info[0])
+
+    @mock.patch.object(eo.Event, 'get_all')
+    def test_event_list2_with_Auth(self, mock_load):
+        mock_load.return_value = []
+
+        req = oreo.EventListRequest(project_safe=True)
+        result = self.eng.event_list2(self.ctx, req.obj_to_primitive())
+        self.assertEqual([], result)
+        mock_load.assert_called_once_with(self.ctx, project_safe=True)
+
+        self.ctx.is_admin = True
+
+        mock_load.reset_mock()
+        req = oreo.EventListRequest(project_safe=True)
+        result = self.eng.event_list2(self.ctx, req.obj_to_primitive())
+        self.assertEqual([], result)
+        mock_load.assert_called_once_with(self.ctx, project_safe=True)
+
+        mock_load.reset_mock()
+        req = oreo.EventListRequest(project_safe=False)
+        result = self.eng.event_list2(self.ctx, req.obj_to_primitive())
+        self.assertEqual([], result)
+        mock_load.assert_called_once_with(self.ctx, project_safe=False)
 
     @mock.patch.object(service.EngineService, 'event_find')
     def test_event_get(self, mock_find):
