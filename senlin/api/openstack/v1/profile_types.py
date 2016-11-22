fba443afa6f3b43a3081c648bcc6ee934323d2ba
lvdongbing <dongbing.lv@kylin-cloud.com>
2016-11-22 04:27:03 -0500

    API support for profile_type_get2
    This patch adds support to profile type get v2 in API layer.
    
    Change-Id: Iec378fd0de5015b2a3380c71fdc740ff8bd03bd5

--- senlin/api/openstack/v1/profile_types.py
+++ senlin/api/openstack/v1/profile_types.py
@@ -14,8 +14,14 @@
 Profile type endpoint for Senlin v1 ReST API.
 """
 
+import jsonschema
+import six
+from webob import exc
+
 from senlin.api.common import util
 from senlin.api.common import wsgi
+from senlin.objects import base as obj_base
+from senlin.objects.requests import profile_type as vorp
 
 
 class ProfileTypeController(wsgi.Controller):
@@ -32,5 +38,15 @@ class ProfileTypeController(wsgi.Controller):
     @util.policy_enforce
     def get(self, req, type_name):
         """Gets the details about a specified profile type."""
-        content = self.rpc_client.profile_type_get(req.context, type_name)
+        try:
+            norm_req = obj_base.SenlinObject.normalize_req(
+                'ProfileTypeGetRequest', {'type_name': type_name})
+            obj = vorp.ProfileTypeGetRequest.obj_from_primitive(norm_req)
+            jsonschema.validate(norm_req, obj.to_json_schema())
+        except ValueError as ex:
+            raise exc.HTTPBadRequest(six.text_type(ex))
+        except jsonschema.exceptions.ValidationError as ex:
+            raise exc.HTTPBadRequest(six.text_type(ex.message))
+
+        content = self.rpc_client.call2(req.context, 'profile_type_get2', obj)
         return {'profile_type': content}
