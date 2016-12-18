1c22df6f1477349e58af977d85c7851de99d617f
RUIJIE YUAN <cnjie0616@gmail.com>
2016-12-18 14:25:53 +0800

    use util method to parse request object in policy_types
    this patch revise the api to use util method to parse request
    obj in policy_types.
    
    Change-Id: Iff5cab7fe8002488ddba072fea86dea797d714ef

--- senlin/api/openstack/v1/policy_types.py
+++ senlin/api/openstack/v1/policy_types.py
@@ -14,14 +14,8 @@
 Policy type endpoint for Senlin v1 ReST API.
 """
 
-import jsonschema
-import six
-from webob import exc
-
 from senlin.api.common import util
 from senlin.api.common import wsgi
-from senlin.objects import base as obj_base
-from senlin.objects.requests import policy_type as orpt
 
 
 class PolicyTypeController(wsgi.Controller):
@@ -33,31 +27,16 @@ class PolicyTypeController(wsgi.Controller):
     @util.policy_enforce
     def index(self, req):
         """Gets the supported policy types"""
-        norm_req = obj_base.SenlinObject.normalize_req(
-            'PolicyTypeListRequest', {})
-        obj = None
-        try:
-            obj = orpt.PolicyTypeListRequest.obj_from_primitive(norm_req)
-            jsonschema.validate(norm_req, obj.to_json_schema())
-        except ValueError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex))
 
+        obj = util.parse_request('PolicyTypeListRequest', req, {})
         types = self.rpc_client.call2(req.context, 'policy_type_list2', obj)
         return {'policy_types': types}
 
     @util.policy_enforce
     def get(self, req, type_name):
         """Gets detailed information for a policy-type"""
-        norm_req = obj_base.SenlinObject.normalize_req(
-            'PolicyTypeGetRequest', {'type_name': type_name})
-        obj = None
-        try:
-            obj = orpt.PolicyTypeGetRequest.obj_from_primitive(norm_req)
-            jsonschema.validate(norm_req, obj.to_json_schema())
-        except ValueError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex))
-        except jsonschema.exceptions.ValidationError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex.message))
 
+        obj = util.parse_request(
+            'PolicyTypeGetRequest', req, {'type_name': type_name})
         content = self.rpc_client.call2(req.context, 'policy_type_get2', obj)
         return {'policy_type': content}
--- senlin/tests/unit/api/openstack/v1/test_policy_types.py
+++ senlin/tests/unit/api/openstack/v1/test_policy_types.py
@@ -14,11 +14,11 @@ import mock
 import six
 from webob import exc
 
+from senlin.api.common import util
 from senlin.api.middleware import fault
 from senlin.api.openstack.v1 import policy_types
 from senlin.common import exception as senlin_exc
 from senlin.common import policy
-from senlin.objects.requests import policy_type as orpt
 from senlin.rpc import client as rpc_client
 from senlin.tests.unit.api import shared
 from senlin.tests.unit.common import base
@@ -35,24 +35,26 @@ class PolicyTypeControllerTest(shared.ControllerTest, base.SenlinTestCase):
         cfgopts = DummyConfig()
         self.controller = policy_types.PolicyTypeController(options=cfgopts)
 
-    def test_policy_type_list(self, mock_enforce):
+    @mock.patch.object(util, 'parse_request')
+    @mock.patch.object(rpc_client.EngineClient, 'call2')
+    def test_policy_type_list(self, mock_call, mock_parse, mock_enforce):
         self._mock_enforce_setup(mock_enforce, 'index', True)
         req = self._get('/policy_types')
 
         engine_response = [{'name': 'os.heat.stack'},
                            {'name': 'os.nova.server'}]
 
-        mock_call = self.patchobject(rpc_client.EngineClient, 'call2',
-                                     return_value=engine_response)
+        mock_call.return_value = engine_response
+        obj = mock.Mock()
+        mock_parse.return_value = obj
 
         response = self.controller.index(req)
-        self.assertEqual({'policy_types': engine_response}, response)
 
+        self.assertEqual(engine_response, response['policy_types'])
+        mock_parse.assert_called_once_with(
+            'PolicyTypeListRequest', req, {})
         mock_call.assert_called_once_with(
             req.context, 'policy_type_list2', mock.ANY)
-
-        request = mock_call.call_args[0][2]
-        self.assertIsInstance(request, orpt.PolicyTypeListRequest)
 
     def test_policy_type_list_err_denied_policy(self, mock_enforce):
         self._mock_enforce_setup(mock_enforce, 'index', False)
@@ -64,7 +66,9 @@ class PolicyTypeControllerTest(shared.ControllerTest, base.SenlinTestCase):
         self.assertEqual(403, resp.status_int)
         self.assertIn('403 Forbidden', six.text_type(resp))
 
-    def test_policy_type_get(self, mock_enforce):
+    @mock.patch.object(util, 'parse_request')
+    @mock.patch.object(rpc_client.EngineClient, 'call2')
+    def test_policy_type_get(self, mock_call, mock_parse, mock_enforce):
         self._mock_enforce_setup(mock_enforce, 'get', True)
         type_name = 'SimplePolicy'
         req = self._get('/policy_types/%(type)s' % {'type': type_name})
@@ -77,26 +81,27 @@ class PolicyTypeControllerTest(shared.ControllerTest, base.SenlinTestCase):
             },
         }
 
-        mock_call = self.patchobject(rpc_client.EngineClient, 'call2',
-                                     return_value=engine_response)
+        mock_call.return_value = engine_response
+        obj = mock.Mock()
+        mock_parse.return_value = obj
 
         response = self.controller.get(req, type_name=type_name)
 
+        self.assertEqual(engine_response, response['policy_type'])
+        mock_parse.assert_called_once_with(
+            'PolicyTypeGetRequest', req, {'type_name': type_name})
         mock_call.assert_called_once_with(
             req.context, 'policy_type_get2', mock.ANY)
 
-        self.assertEqual(engine_response, response['policy_type'])
-        request = mock_call.call_args[0][2]
-        self.assertIsInstance(request, orpt.PolicyTypeGetRequest)
-        self.assertEqual('SimplePolicy', request.type_name)
-
-    def test_policy_type_get_not_found(self, mock_enforce):
+    @mock.patch.object(util, 'parse_request')
+    @mock.patch.object(rpc_client.EngineClient, 'call2')
+    def test_policy_type_get_not_found(self, mock_call, mock_parse,
+                                       mock_enforce):
         self._mock_enforce_setup(mock_enforce, 'get', True)
         type_name = 'BogusPolicyType'
         req = self._get('/policy_types/%(type)s' % {'type': type_name})
 
         error = senlin_exc.ResourceNotFound(type='policy_type', id=type_name)
-        mock_call = self.patchobject(rpc_client.EngineClient, 'call2')
         mock_call.side_effect = shared.to_remote_error(error)
 
         resp = shared.request_with_middleware(fault.FaultWrapper,
@@ -105,16 +110,21 @@ class PolicyTypeControllerTest(shared.ControllerTest, base.SenlinTestCase):
         self.assertEqual(404, resp.json['code'])
         self.assertEqual('ResourceNotFound', resp.json['error']['type'])
 
+    @mock.patch.object(util, 'parse_request')
     @mock.patch.object(rpc_client.EngineClient, 'call2')
-    def test_policy_type_get_bad_param(self, mock_call, mock_enforce):
+    def test_policy_type_get_bad_param(self, mock_call, mock_parse,
+                                       mock_enforce):
         self._mock_enforce_setup(mock_enforce, 'get', True)
         type_name = 11
         req = self._get('/policy_types/%(type)s' % {'type': type_name})
 
+        mock_parse.side_effect = exc.HTTPBadRequest("bad param")
         ex = self.assertRaises(exc.HTTPBadRequest,
                                self.controller.get,
                                req, type_name=type_name)
-        self.assertEqual("11 is not of type 'string'", six.text_type(ex))
+        self.assertEqual("bad param", six.text_type(ex))
+        mock_parse.assert_called_once_with(
+            'PolicyTypeGetRequest', req, {'type_name': type_name})
         self.assertEqual(0, mock_call.call_count)
 
     def test_policy_type_schema_err_denied_policy(self, mock_enforce):
