ae429ec7b91bdc452ddd2d9caaa393123166f2a8
RUIJIE YUAN <cnjie0616@gmail.com>
2016-12-19 11:18:44 +0800

    revise profile api
    this patch revises profile api to use util method to parse
    request object.
    
    Change-Id: I930fcc4b796913dd753ef4ade7ab4dd9ebc57a0c

--- senlin/api/openstack/v1/profiles.py
+++ senlin/api/openstack/v1/profiles.py
@@ -14,8 +14,6 @@
 Profile endpoint for Senlin v1 ReST API.
 """
 
-import jsonschema
-import six
 from webob import exc
 
 from senlin.api.common import util
@@ -23,7 +21,6 @@ from senlin.api.common import wsgi
 from senlin.common import consts
 from senlin.common.i18n import _
 from senlin.objects import base as obj_base
-from senlin.objects.requests import profiles as vorp
 
 
 class ProfileController(wsgi.Controller):
@@ -53,64 +50,31 @@ class ProfileController(wsgi.Controller):
             params.pop(consts.PARAM_GLOBAL_PROJECT, False))
         params['project_safe'] = project_safe
 
-        try:
-            norm_req = obj_base.SenlinObject.normalize_req(
-                'ProfileListRequest', params)
-            obj = vorp.ProfileListRequest.obj_from_primitive(norm_req)
-            jsonschema.validate(norm_req, obj.to_json_schema())
-        except ValueError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex))
-        except jsonschema.exceptions.ValidationError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex.message))
-
+        obj = util.parse_request('ProfileListRequest', req, params)
         profiles = self.rpc_client.call2(req.context, 'profile_list2', obj)
         return {'profiles': profiles}
 
     @util.policy_enforce
     def create(self, req, body):
-        try:
-            norm_req = obj_base.SenlinObject.normalize_req(
-                'ProfileCreateRequest', body, 'profile')
-            obj = vorp.ProfileCreateRequest.obj_from_primitive(norm_req)
-            jsonschema.validate(norm_req, obj.to_json_schema())
-        except ValueError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex))
-        except jsonschema.exceptions.ValidationError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex.message))
 
+        obj = util.parse_request('ProfileCreateRequest', req, body, 'profile')
         result = self.rpc_client.call2(req.context, 'profile_create2', obj)
         return {'profile': result}
 
     @wsgi.Controller.api_version('1.2')
     @util.policy_enforce
     def validate(self, req, body):
-        try:
-            norm_req = obj_base.SenlinObject.normalize_req(
-                'ProfileValidateRequest', body, 'profile')
 
-            obj = vorp.ProfileValidateRequest.obj_from_primitive(norm_req)
-            jsonschema.validate(norm_req, obj.to_json_schema())
-        except ValueError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex))
-        except jsonschema.exceptions.ValidationError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex.message))
-
+        obj = util.parse_request(
+            'ProfileValidateRequest', req, body, 'profile')
         result = self.rpc_client.call2(req.context, 'profile_validate2', obj)
         return {'profile': result}
 
     @util.policy_enforce
     def get(self, req, profile_id):
         params = {'identity': profile_id}
-        try:
-            norm_req = obj_base.SenlinObject.normalize_req(
-                'ProfileGetRequest', params)
-            obj = vorp.ProfileGetRequest.obj_from_primitive(norm_req)
-            jsonschema.validate(norm_req, obj.to_json_schema())
-        except ValueError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex))
-        except jsonschema.exceptions.ValidationError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex.message))
 
+        obj = util.parse_request('ProfileGetRequest', req, params)
         profile = self.rpc_client.call2(req.context, 'profile_get2', obj)
         return {'profile': profile}
 
@@ -121,33 +85,19 @@ class ProfileController(wsgi.Controller):
             raise exc.HTTPBadRequest(_("Malformed request data, missing "
                                        "'profile' key in request body."))
 
-        try:
-            body_req = obj_base.SenlinObject.normalize_req(
-                'ProfileUpdateRequestBody', profile_data)
-            norm_req = obj_base.SenlinObject.normalize_req(
-                'ProfileUpdateRequest', {'identity': profile_id,
-                                         'profile': body_req})
-            obj = vorp.ProfileUpdateRequest.obj_from_primitive(norm_req)
-            jsonschema.validate(norm_req, obj.to_json_schema())
-        except ValueError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex))
-        except jsonschema.exceptions.ValidationError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex.message))
+        body_req = obj_base.SenlinObject.normalize_req(
+            'ProfileUpdateRequestBody', profile_data)
+        obj = util.parse_request(
+            'ProfileUpdateRequest', req, {'identity': profile_id,
+                                          'profile': body_req})
 
         profile = self.rpc_client.call2(req.context, 'profile_update2', obj)
         return {'profile': profile}
 
     @util.policy_enforce
     def delete(self, req, profile_id):
-        try:
-            norm_req = obj_base.SenlinObject.normalize_req(
-                'ProfileDeleteRequest', {'identity': profile_id})
-            obj = vorp.ProfileDeleteRequest.obj_from_primitive(norm_req)
-            jsonschema.validate(norm_req, obj.to_json_schema())
-        except ValueError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex))
-        except jsonschema.exceptions.ValidationError as ex:
-            raise exc.HTTPBadRequest(six.text_type(ex.message))
 
+        obj = util.parse_request(
+            'ProfileDeleteRequest', req, {'identity': profile_id})
         self.rpc_client.call2(req.context, 'profile_delete2', obj)
         raise exc.HTTPNoContent()
