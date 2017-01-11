c5ed38633afb095d5c1054ba46b8a0a78e0cf46a
miaohb <miao.hongbao@zte.com.cn>
2017-01-11 13:25:01 +0800

    Return support status in profile type show
    This patch enables senlin engine/api to return support status in profile
    type show. This feature needs to be enabled by API micro-version
    1.5.
    
    Change-Id: I7a4bcdaaa857d776d926bbbfde3c517f05d8935b

--- senlin/tests/tempest/api/profile_types/test_profile_type_show.py
+++ senlin/tests/tempest/api/profile_types/test_profile_type_show.py
@@ -13,10 +13,12 @@
 from tempest.lib import decorators
 
 from senlin.tests.tempest.api import base
+from senlin.tests.tempest.common import utils
 
 
 class TestProfileTypeShow(base.BaseSenlinAPITest):
 
+    @utils.api_microversion('1.4')
     @decorators.idempotent_id('198165b3-1c1f-4801-8918-90c1adbf57c8')
     def test_profile_type_show(self):
         res = self.client.get_obj('profile-types', 'os.nova.server-1.0')
@@ -28,3 +30,17 @@ class TestProfileTypeShow(base.BaseSenlinAPITest):
         for key in ['name', 'schema']:
             self.assertIn(key, profile_type)
         self.assertEqual('os.nova.server-1.0', profile_type['name'])
+
+    @utils.api_microversion('1.5')
+    @decorators.idempotent_id('778d41df-0ce0-421f-98e5-2efdcec6d995')
+    def test_profile_type_show_v1_5(self):
+        res = self.client.get_obj('profile-types', 'os.nova.server-1.0')
+
+        # Verify resp of profile type show API
+        self.assertEqual(200, res['status'])
+        self.assertIsNotNone(res['body'])
+        profile_type = res['body']
+        for key in ['name', 'schema', 'support_status']:
+            self.assertIn(key, profile_type)
+        self.assertEqual('os.nova.server-1.0', profile_type['name'])
+        self.assertIsNotNone(profile_type['support_status'])
