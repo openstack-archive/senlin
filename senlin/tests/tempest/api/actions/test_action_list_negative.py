c2f0a3fa4b46943750aa673397f2d8398afcacf4
RUIJIE YUAN <cnjie0616@gmail.com>
2017-01-16 10:04:20 +0800

    improve tempest test for action list
    This is the first patch to improve the tempest test which
    will assert the exception message to make sure it is what we
    want.
    
    And also, hope the test itself is what we want.
    
    Change-Id: I7eaa1d9402263870b2d3021f05a110677b959931

--- senlin/tests/tempest/api/actions/test_action_list_negative.py
+++ senlin/tests/tempest/api/actions/test_action_list_negative.py
@@ -22,6 +22,66 @@ class TestActionListNegativeBadRequest(base.BaseSenlinAPITest):
     @test.attr(type=['negative'])
     @decorators.idempotent_id('2f857bee-99a8-4881-bde9-5909e3ff121a')
     def test_action_list_invalid_params(self):
-        self.assertRaises(exceptions.BadRequest,
-                          self.client.list_objs,
-                          'actions', {'bogus': 'foo'})
+        ex = self.assertRaises(exceptions.BadRequest,
+                               self.client.list_objs,
+                               'actions', {'bogus': 'foo'})
+
+        message = ex.resp_body['error']['message']
+        self.assertEqual("Invalid parameter bogus", str(message))
+
+    @test.attr(type=['negative'])
+    @decorators.idempotent_id('6b93221e-ca14-443b-be54-4423a636672a')
+    def test_action_list_limit_not_int(self):
+        ex = self.assertRaises(exceptions.BadRequest,
+                               self.client.list_objs,
+                               'actions', {'limit': 'not-int'})
+
+        message = ex.resp_body['error']['message']
+        self.assertEqual("invalid literal for int() with base 10: 'not-int'",
+                         str(message))
+
+    @test.attr(type=['negative'])
+    @decorators.idempotent_id('d13e3011-32b5-4f9d-bb57-3532c4c46228')
+    def test_action_list_global_project_false(self):
+        ex = self.assertRaises(exceptions.Forbidden,
+                               self.client.list_objs,
+                               'actions', {'global_project': 'True'})
+
+        message = ex.resp_body['error']['message']
+        self.assertEqual("You are not authorized to complete this operation.",
+                         str(message))
+
+    @test.attr(type=['negative'])
+    @decorators.idempotent_id('d7804b5e-6efd-4084-adec-531d0a8399dc')
+    def test_action_list_global_project_not_bool(self):
+        ex = self.assertRaises(exceptions.BadRequest,
+                               self.client.list_objs,
+                               'actions', {'global_project': 'not-bool'})
+
+        message = ex.resp_body['error']['message']
+        self.assertEqual("Invalid value 'not-bool' specified for "
+                         "'global_project'", str(message))
+
+    @test.attr(type=['negative'])
+    @decorators.idempotent_id('6ca48051-6030-4333-9483-074e03da5ba0')
+    def test_action_list_invalid_sort(self):
+        ex = self.assertRaises(exceptions.BadRequest,
+                               self.client.list_objs,
+                               'actions', {'sort': 'bad-sort'})
+
+        message = ex.resp_body['error']['message']
+        self.assertEqual("unsupported sort key 'bad-sort' for 'sort'.",
+                         str(message))
+
+    @test.attr(type=['negative'])
+    @decorators.idempotent_id('588517ed-bfcf-4d92-9d7c-5ed11fc2c2ee')
+    def test_action_list_invalid_marker(self):
+        ex = self.assertRaises(exceptions.BadRequest,
+                               self.client.list_objs,
+                               'actions', {'marker': 'bad-marker'})
+
+        message = ex.resp_body['error']['message']
+        self.assertEqual(
+            "u'bad-marker' does not match '^[a-fA-F0-9]{8}-?[a-fA-F0-9]"
+            "{4}-?[a-fA-F0-9]{4}-?[a-fA-F0-9]{4}-?[a-fA-F0-9]{12}$'",
+            str(message))
