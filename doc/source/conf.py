a97a6042538dcb40ca2be9c721696bf1b38b291c
melissaml <ma.lei@99cloud.net>
2017-02-08 17:23:14 +0800

    Use https instead of http for git.openstack.org
    Trivialfix
    
    Change-Id: I6f88ebfa5d9b833f14b551b393755764d90e4177

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -68,7 +68,7 @@ version = senlin_version.canonical_version_string()
 #   bug_tag: Tag for categorizing the bug. Must be set manually.
 #   bug_project: Launchpad project to file bugs against.
 # These variables are passed to the logabug code via html_context.
-giturl = u'http://git.openstack.org/cgit/openstack/senlin/tree/doc/source'
+giturl = u'https://git.openstack.org/cgit/openstack/senlin/tree/doc/source'
 git_cmd = ["git", "log", "--pretty=format:'%ad, commit %h", "--date=local",
            "-n1"]
 gitsha = subprocess.Popen(git_cmd,
